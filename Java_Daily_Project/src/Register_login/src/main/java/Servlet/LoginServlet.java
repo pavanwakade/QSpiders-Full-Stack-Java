7144d305ca6b8618c7324bd758c69af7b56d5dd1
pavan wakade <pavanwakade199@gmail.com>
2025-01-20 10:09:32 +0530

    Register  and Login Using Postgresql database,servlet and HTML

---
+++ Java_Daily_Project/src/Register_login/src/main/java/Servlet/LoginServlet.java
@@ -0,0 +1,20 @@
+package Servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet {
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String mob = req.getParameter("mob");
+		String email = req.getParameter("email");
+		String pass = req.getParameter("pass");
+	}
+
+}
