49eb447150c25e502c74c3526c28e0f188c744f7
pavan wakade <pavanwakade199@gmail.com>
2025-01-28 10:07:58 +0530

    Bidirectional OneToOneMapping

---
+++ hibernate/PostgreSQL/bidirectionalOneToOneMapping/DeleteData.java
@@ -0,0 +1,58 @@
+package bidirectionalOneToOneMapping;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+
+public class DeleteData {
+
+	public static void main(String[] args) {
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("pavan");
+
+		EntityManager em = emf.createEntityManager();
+
+		EntityTransaction et = em.getTransaction();
+
+		Person person = em.find(Person.class, 1);
+
+		if (person != null) {
+
+			et.begin();
+
+			em.remove(person);
+
+			System.out.println("Account deleted!!");
+
+			et.commit();
+			
+			
+		}
+
+		else {
+			System.out.println("Account Not Fount!!");
+		}
+		
+		
+		
+		Adhar account = em.find(Adhar.class, 101);
+		if (account != null) {
+
+			et.begin();
+
+			em.remove(account);
+
+			System.out.println("Account deleted!!");
+
+			et.commit();
+			
+			
+		}
+
+		else {
+			System.out.println("Account Not Fount!!");
+		}
+	}
+
+}
