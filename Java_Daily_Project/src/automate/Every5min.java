e37dcb8f8e9db643b38aab4ff69ac709927b3ded
pavanwakade <pavanwakade199@gmail.com>
2025-03-20 10:29:31 +0530

    updateed

--- Java_Daily_Project/src/automate/Every5min.java
+++ Java_Daily_Project/src/automate/Every5min.java
@@ -29,7 +29,9 @@ public class Every5min {
         if (args.length >= 2) {
             commitMessage = args[1];
         } else {
-            commitMessage = "auto-update " + System.currentTimeMillis();
+            commitMessage = "updateed";
+//            commitMessage = "auto-update " + System.currentTimeMillis();
+
         }
 
         ScheduledExecutorService scheduler = Executors.newSingleThreadScheduledExecutor();
