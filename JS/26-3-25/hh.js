364f251c2bb91fd1fe08c059cba2323e22ac1ea1
pavanwakade <pavanwakade199@gmail.com>
2025-03-26 14:54:47 +0530

    updated

---
+++ JS/26-3-25/hh.js
@@ -0,0 +1,16 @@
+
+let start = prompt("enter start number");
+let end = prompt("enter End number");
+
+let checkEven = (a, b) => {
+
+    for (; a <= b; a++) {
+
+        if (a % 2 == 0) {
+
+            console.log(a);
+        }
+    }
+}
+
+checkEven(start, end);
