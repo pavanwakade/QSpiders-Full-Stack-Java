2e75e2943c0aa3538f7099bbad72de07f39b9042
pavanwakade <pavanwakade199@gmail.com>
2025-03-27 12:41:16 +0530

    updated

--- Java_Daily_Project/src/automate/ff.java
+++ Java_Daily_Project/src/automate/ff.java
@@ -4,9 +4,6 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.Executors;
@@ -18,6 +15,7 @@ import java.util.logging.Logger;
 import java.util.logging.SimpleFormatter;
 
 public class ff {
+	
     private static final int COMMIT_INTERVAL_MINUTES = 1; // Changed to 5 minutes
     private static final String DEFAULT_COMMIT_MESSAGE = "Auto commit";
     private static final Logger LOGGER = Logger.getLogger(ff.class.getName());
