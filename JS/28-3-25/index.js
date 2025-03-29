3f7dd3b7bdc44e538ae91aa94637e1c5014cfb6d
pavanwakade <pavanwakade199@gmail.com>
2025-03-29 13:49:08 +0530

    Updated

--- JS/28-3-25/index.js
+++ JS/28-3-25/index.js
@@ -1,80 +1,91 @@
-// 1. Callback Function
-function shout(message, callback) {
-    callback(message);
-}
-shout("hello", (msg) => console.log(msg.toUpperCase()));
+// // 1. Callback Function
+// function shout(message, callback) {
+//     callback(message);
+// }
+// shout("hello", (msg) => console.log(msg.toUpperCase()));
 
-// 2. Friendly Robot
-function robot(name, callback) {
-    console.log(callback(name));
-}
-robot("Alice", (name) => `Hello, ${name}! I am your friendly robot.`);
+// // 2. Friendly Robot
+// function robot(name, callback) {
+//     console.log(callback(name));
+// }
+// robot("Alice", (name) => `Hello, ${name}! I am your friendly robot.`);
 
-// 3. Math Wizard
-function mathWizard(a, b, callback) {
-    console.log(callback(a, b));
-}
-mathWizard(5, 3, (x, y) => {
-   return x + y
-}
-);
-// 4
-let decide = (msg, yesCallback, noCallback) => {
-    if (msg === "happy") {
-        console.log(yesCallback());
-    } else {
-        console.log(noCallback()); 
-    }
-}
+// // 3. Math Wizard
+// function mathWizard(a, b, callback) {
+//     console.log(callback(a, b));
+// }
+// mathWizard(5, 3, (x, y) => {
+//     return x + y
+// }
+// );
+// // 4 
+// let decide = (msg, yesCallback, noCallback) => {
+//     if (msg === "happy") {
+//         console.log(yesCallback());
+//     } else {
+//         console.log(noCallback());
+//     }
+// }
 
-let yes = () => "Yay!";
+// let yes = () => "Yay!";
 
-let no = () => "Oh no!";
-decide("happy",yes,no);
+// let no = () => "Oh no!";
+// decide("happy", yes, no);
 
-// decide("sad",yes,no);
+// // decide("sad",yes,no);
 
- 
 
-// 5. Double the Fun
-let doubleFun =(num, callback) => console.log(callback(num));
 
-doubleFun(4, (n) => n * 2);
+// // 5. Double the Fun
+// let doubleFun = (num, callback) => console.log(callback(num));
 
-// 6. Compliment Machine
-function complimentMachine(name, callback) {
-    console.log(callback(name));
-}
-let awesome = (name) => `${name}, you are awesome!`;
-complimentMachine("Sam",awesome);
+// doubleFun(4, (n) => n * 2);
 
-// 7. Checker Board
-function checkNumber(num, callback) {
-    console.log(callback(num));
-}
-let check=(num)=> num%2==0 ? "Even Number" : "Odd Number";
-checkNumber(10,check);
+// // 6. Compliment Machine
+// function complimentMachine(name, callback) {
+//     console.log(callback(name));
+// }
+// let awesome = (name) => `${name}, you are awesome!`;
+// complimentMachine("Sam", awesome);
 
-// 8. Number Judge
-let judgeNumber = (num, callback) => console.log(callback(num));
-judgeNumber(-5, (n) => n > 0 ? "Positive" : "Negative");
+// // 7. Checker Board
+// function checkNumber(num, callback) {
+//     console.log(callback(num));
+// }
+// let check = (num) => num % 2 == 0 ? "Even Number" : "Odd Number";
+// checkNumber(10, check);
 
-// 9. Tiny Calculator
-let cal = (a, b, callback) =>  console.log(callback(a, b));
+// // 8. Number Judge
+// let judgeNumber = (num, callback) => console.log(callback(num));
+// judgeNumber(-5, (n) => n > 0 ? "Positive" : "Negative");
 
-cal(8, 2, (x, y) => (x * y));
+// // 9. Tiny Calculator
+// let cal = (a, b, callback) => console.log(callback(a, b));
 
-// 10. Name Styler
-function nameStyler(name, callback) {
-    console.log(callback(name));
-}
-nameStyler("Charlie", (name) => `***${name.toUpperCase()}***`);
+// cal(8, 2, (x, y) => (x * y));
+
+// // 10. Name Styler
+// function nameStyler(name, callback) {
+//     console.log(callback(name));
+// }
+// nameStyler("Charlie", (name) => `***${name.toUpperCase()}***`);
+
+
 
 // 11. Number Transformer
-function transform(num, callback) {
-    console.log(callback(num));
-}
-transform(3, (n) => n ** 2);
+
+let transform = (num, operation) => {
+    if (operation === 'square') {
+        return console.log(`Square of ${num} :`,num * num);
+    } else {
+        return console.log(`Cube of ${num} :`,num * num * num);
+    }
+};
+
+transform(3, 'square');
+
+
+
 
 // 12. Ask the Oracle
 function askOracle(question, callback) {
