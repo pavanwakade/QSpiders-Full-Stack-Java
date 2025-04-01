fb6768ae769c5d517105613ada9c0b429ceecae3
pavanwakade <pavanwakade199@gmail.com>
2025-04-01 19:45:17 +0530

    Updated

--- JS/jj.js
+++ JS/jj.js
@@ -1,6 +1,6 @@
 
-// // 15. Magic Mirror
-// function magicMirror(word, callback) {
-//     console.log(callback(word));
-// }
-// magicMirror("hello", (w) => w.split('').reverse().join(''));
+// 15. Magic Mirror
+function magicMirror(word, callback) {
+    console.log(callback(word));
+}
+magicMirror("hello", (w) => w.split('').reverse().join(''));
