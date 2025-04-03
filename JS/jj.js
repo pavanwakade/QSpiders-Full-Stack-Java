f271c820c9a3025fcab44310b7ad282bb3028bfa
pavanwakade <pavanwakade199@gmail.com>
2025-04-03 15:27:39 +0530

    Updated

--- JS/jj.js
+++ JS/jj.js
@@ -1,6 +1,12 @@
 
 // 15. Magic Mirror
-function magicMirror(word, callback) {
-    console.log(callback(word));
-}
-magicMirror("hello", (w) => w.split('').reverse().join(''));
+// function magicMirror(word, callback) {
+//     console.log(callback(word));
+// }
+// magicMirror("hello", (w) => w.split('').reverse().join(''));
+
+
+
+// regulr functionget voicestate
+
+
