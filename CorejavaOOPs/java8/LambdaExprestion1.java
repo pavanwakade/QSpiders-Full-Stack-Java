08a45266dcccb5bda54f0c51c34b858bd11886bc
pavanwakade <pavanwakade199@gmail.com>
2025-04-07 08:39:59 +0530

    Updated

---
+++ CorejavaOOPs/java8/LambdaExprestion1.java
@@ -0,0 +1,14 @@
+package java8;
+//
+//interface calculator {
+//	void add();
+//}
+
+public class LambdaExprestion1 {
+
+	public static void main(String[] args) {
+
+		Runnable r = () -> System.out.println("hhh");
+
+	}
+}
