4339b2600e655769dbc8fbf6cc3eb18766f5f93b
pavanwakade <pavanwakade199@gmail.com>
2025-04-07 09:41:38 +0530

    Updated

--- CorejavaOOPs/java8/Calculator.java
+++ CorejavaOOPs/java8/Calculator.java
@@ -14,7 +14,10 @@ public class Calculator {
 		calculate mul = (a, b) -> a * b;
 		calculate div = (a, b) -> a == 0 || b == 0 ? 0 : a / b;
 
-		System.out.println(add.a(1, 1));
-		System.out.println(sub.a(4, 5));
+		System.out.println("addition      : " + add.a(10, 1));
+		System.out.println("substraction  : " + sub.a(4, 5));
+		System.out.println("Multiplication: " + mul.a(4, 5));
+		System.out.println("division      : " + div.a(4, 5));
+		System.out.println("division      : " + div.a(4, 0));
 	}
 }
