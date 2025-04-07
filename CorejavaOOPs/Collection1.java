2f1db6521bef772cf98654165365a2a7320980e3
pavanwakade <pavanwakade199@gmail.com>
2025-04-07 22:23:02 +0530

    Updated

---
+++ CorejavaOOPs/Collection1.java
@@ -0,0 +1,7 @@
+class Collection1 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello World!");
+	}
+}
---
+++ CorejavaOOPs/collection/collectioninterface/Collection1.java
@@ -0,0 +1,9 @@
+package collection.collectioninterface;
+
+public class Collection1 {
+
+	public static void main(String[] args) {
+	
+		
+	}
+}
