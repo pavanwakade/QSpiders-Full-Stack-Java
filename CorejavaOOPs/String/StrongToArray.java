0a661c90e7d0ff56df50433abb32d8d3bff7af76
pavanwakade <pavanwakade199@gmail.com>
2025-04-08 15:21:45 +0530

    Updated

---
+++ CorejavaOOPs/String/StrongToArray.java
@@ -0,0 +1,24 @@
+package String;
+
+import java.util.Arrays;
+
+public class StrongToArray {
+
+	public static void main(String[] args) {
+
+		String str = "hello pavan";
+
+		char a[] = new char[str.length()];
+
+		for (int i = 0; i < str.length(); i++) {
+
+			a[i] = str.charAt(i);
+		}
+
+		System.out.println(Arrays.toString(a));
+
+		String[] c = str.split("");
+		System.out.println(Arrays.toString(c));
+
+	}
+}
