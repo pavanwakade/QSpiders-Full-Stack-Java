d0f6666915817f41d29d918e38df0bd237c4e9c9
pavanwakade <pavanwakade199@gmail.com>
2025-04-09 14:17:15 +0530

    Updated

--- JS/9-4-25/array.js
+++ JS/9-4-25/array.js
@@ -4,7 +4,7 @@ let f = (a, b) => a + b;
 
 let a = [1, 2, 3, "hello", null, NaN, f, undefined];
 
-console.log(a);
+// console.log(a);
 
 // console.log(a[6](4,7));
 
@@ -19,3 +19,12 @@ for (let i = 0; i < a.length; i++) {
 //MAP
 
 let num = [1, 3, 4, 6, 8, 0, 2];
+
+let apnamap = (arr)=>
+    for (let i = 0; i < arr.length; i++) {
+       
+        
+    }
+
+
+// num.map((val) => console.log(val));
