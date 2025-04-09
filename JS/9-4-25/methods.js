3bd067861155645faa3451d32395d181217981fc
pavanwakade <pavanwakade199@gmail.com>
2025-04-09 08:07:27 +0530

    Updated

---
+++ JS/9-4-25/methods.js
@@ -0,0 +1,17 @@
+let str=" Good Morning Sir My Name is Pavan ";
+
+let friend = "Kedar,Shashi"
+
+// console.log(str);
+
+// console.log(str.toLowerCase());
+
+// console.log(str.toUpperCase());
+
+// console.log(str.trim());
+
+console.log(str.replace("pavan",friend));
+
+
+
+
