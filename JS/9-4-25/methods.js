7743d454eef60a9bf7ca036f35f0988065f78ee4
pavanwakade <pavanwakade199@gmail.com>
2025-04-09 08:39:36 +0530

    Updated

--- JS/9-4-25/methods.js
+++ JS/9-4-25/methods.js
@@ -10,7 +10,36 @@ let friend = "Kedar,Shashi"
 
 // console.log(str.trim());
 
-console.log(str.replace("pavan",friend));
+// console.log(str.replace("Pavan",friend));
+
+// console.log(str.split(''))
+
+// let t=str.split('');
+
+// console.log(t.join(''));
+
+// console.log(str.repeat(2));
+
+// console.log(str.length);
+
+// console.log(str.padEnd(40,"*"));
+
+// console.log(str.padStart(40,"*"));
+
+// console.log(str.startsWith("good"));
+
+// console.log(str.endsWith("good"));
+
+// console.log(str.indexOf("P"));
+
+
+
+
+
+
+
+
+
 
 
 
