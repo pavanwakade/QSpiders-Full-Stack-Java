13bc0e118cbd6e73b8a0968464980ec4c597d658
pavanwakade <pavanwakade199@gmail.com>
2025-04-09 19:44:38 +0530

    Updated

--- CorejavaOOPs/collection/collectioninterface/collectionInterfaceMethods.java
+++ CorejavaOOPs/collection/collectioninterface/collectionInterfaceMethods.java
@@ -1,14 +1,16 @@
 package collection.collectioninterface;
 
+import java.util.Arrays;
+
 public class collectionInterfaceMethods {
 
 	public static void main(String[] args) {
-		String string="hello pavan";
-		
-		String[] string2=string.split(" ");
-		
-				
-//		System.out.println();
-		
+		String string = "hello pavan";
+
+		String[] string2 = string.split("");
+
+		System.out.println(Arrays.toString(string2).join("", string2));
+
+		System.out.println(Arrays.toString(string2));
 	}
 }
