751c29915a2649d57e59a6e0eadef0ad14dd5cda
pavanwakade <pavanwakade199@gmail.com>
2025-04-09 07:37:29 +0530

    Updated

--- CorejavaOOPs/String/StringToArray.java
+++ CorejavaOOPs/String/StringToArray.java
@@ -1,34 +1,56 @@
 package String;
 
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 public class StringToArray {
 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 
-        // Input String
-        String str = "hello pavan";
+		String str = "hello pavan";
 
-        // Character array to store reversed characters
-        char a[] = new char[str.length()];
+		char a[] = new char[str.length()];
 
-        // Pointer to the last index of the array
-        int p = a.length - 1;
+		int p = a.length - 1;
 
-        // String to store the reversed result
-        StringBuilder sb = new StringBuilder();
+		String s = "";
 
-        // Loop to reverse the string
-        for (int i = 0; i < str.length(); i++, p--) {
-            a[p] = str.charAt(i); // Assign characters to the array in reverse order
-            sb.append(a[p]);      // Append reversed characters to the StringBuilder
-        }
+		for (int i = 0; i < str.length(); i++, p--) {
+			a[p] = str.charAt(i);
+		}
+		System.out.println(Arrays.toString(a));
 
-        // Convert StringBuilder to String
-        String s = sb.toString();
+		for (int i = 0; i < a.length; i++) {
+			s = s + a[i];
+		}
+		System.out.println(s);
 
-        // Output the reversed array and string
-        System.out.println(Arrays.toString(a)); // Print reversed character array
-        System.out.println(s);                  // Print reversed string
-    }
+		
+		
+		
+		
+
+		System.out.println();
+		System.out.println("using collection");
+		// Step 1: Split the string into characters
+		String[] c = str.split("");
+
+		System.out.println(Arrays.toString(c));
+
+		// Step 2: Convert the array into a List
+		List<String> list = Arrays.asList(c);
+
+		// Step 3: Reverse the list in place
+		Collections.reverse(list);
+
+		System.out.println(list);
+		
+		// Step 4: Join the reversed list back into a string
+		String reversed = String.join("", list);
+
+		// Print the reversed string
+		System.out.println(reversed);
+
+	}
 }
