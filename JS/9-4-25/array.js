a978e672d2bd157374ca0097111541404ae5a07a
pavanwakade <pavanwakade199@gmail.com>
2025-04-09 14:54:11 +0530

    Updated

--- JS/9-4-25/array.js
+++ JS/9-4-25/array.js
@@ -18,13 +18,38 @@ for (let i = 0; i < a.length; i++) {
 
 //MAP
 
+//Working of map
 let num = [1, 3, 4, 6, 8, 0, 2];
-
-let apnamap = (arr)=>
+let apnamap = (cbf, arr) => {
     for (let i = 0; i < arr.length; i++) {
-       
-        
+        cbf(arr[i])
     }
+}
+
+apnamap((val) => {
+    // console.log(val)
+}, num)
 
 
-// num.map((val) => console.log(val));
+
+//map
+num.map((val) => {
+    // console.log(val)
+}
+);
+
+
+let friends = ["pavan", "shashi", "kedar", "sandip"]
+
+friends.map((val) => {
+    if (val === "pavan") {
+        // console.log(val)
+        return;
+    }
+}
+);
+
+
+
+let numbers = [8, 5, 7, 9, 2, 6, 8];
+numbers.map((val, i) => console.log(val * 100 + 5 + 10, i));
