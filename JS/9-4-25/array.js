3cc5f456ec9f0c430ebc1790e43b6edfac973466
pavanwakade <pavanwakade199@gmail.com>
2025-04-10 13:40:15 +0530

    Updated

--- JS/9-4-25/array.js
+++ JS/9-4-25/array.js
@@ -1,83 +1,109 @@
 
 
-let f = (a, b) => a + b;
+// let f = (a, b) => a + b;
 
-let a = [1, 2, 3, "hello", null, NaN, f, undefined];
+// let a = [1, 2, 3, "hello", null, NaN, f, undefined];
 
-console.log(a);
+// console.log(a);
 
-console.log(a[6](4,7));
-
-
-for (let i = 0; i < a.length; i++) {
-    // console.log(a[i]);
-}
-
-// a.forEach(element => console.log(element));
+// console.log(a[6](4,7));
 
 
-//MAP
-//Working of map
-let num = [1, 3, 4, 6, 8, 0, 2];
-let apnamap = (cbf, arr) => {
+// for (let i = 0; i < a.length; i++) {
+//     // console.log(a[i]);
+// }
+
+// // a.forEach(element => console.log(element));
+
+
+// //MAP
+// //Working of map
+// let num = [1, 3, 4, 6, 8, 0, 2];
+// let apnamap = (cbf, arr) => {
+//     for (let i = 0; i < arr.length; i++) {
+//         cbf(arr[i])
+//     }
+// }
+
+// apnamap((val) => {
+//     // console.log(val)
+// }, num)
+
+
+
+// //map
+// num.map((val) => {
+//     // console.log(val)
+// }
+// );
+
+// let friends = ["pavan", "shashi", "kedar", "sandip"]
+
+// friends.map((val) => {
+//     if (val === "pavan") {
+//         // console.log(val)
+//         return;
+//     }
+// }
+// );
+
+
+// let numbers = [8, 5, 7, 9, 2, 6, 8];
+// numbers.map((val, i) => console.log(val * 100 + 5 + 10, i));
+
+
+
+
+
+
+
+
+
+
+
+// //Map practice 
+
+// let arr = [94, 3, 5, 6, 3, 3, 5, 6, 6,]
+
+// //work of map
+
+// let mapcaoncept = (callback, arr) => {
+//     for (let i = 0; i < arr.length; i++) {
+//         callback(arr[i]);
+//     }
+// }
+
+// mapcaoncept((val) => {
+//     // console.log(val);
+
+// }, arr)
+
+
+
+
+// // map
+// arr.map((val) => console.log(val))
+
+
+
+
+
+let num = [1, 2, 3, 7, 6, 8, 9,];
+
+apnamap = (callback, arr) => {
+
+    let tremparr = [];
     for (let i = 0; i < arr.length; i++) {
-        cbf(arr[i])
+
+        let a = callback(arr[i], i, arr)
+        tremparr.push(a);
     }
+
+    return tremparr;
 }
 
-apnamap((val) => {
-    // console.log(val)
+let b = apnamap((val, index, arr) => {
+    return val;
 }, num)
 
-
-
-//map
-num.map((val) => {
-    // console.log(val)
-}
-);
-
-let friends = ["pavan", "shashi", "kedar", "sandip"]
-
-friends.map((val) => {
-    if (val === "pavan") {
-        // console.log(val)
-        return;
-    }
-}
-);
-
-
-let numbers = [8, 5, 7, 9, 2, 6, 8];
-numbers.map((val, i) => console.log(val * 100 + 5 + 10, i));
-
-
-
-
-
-
-
-
-
-
-
-//Map practice 
-
-let arr = [94, 3, 5, 6, 3, 3, 5, 6, 6,]
-
-//work of map
-
-let mapcaoncept = (callback, arr) => {
-    for (let i = 0; i < arr.length; i++) {
-        callback(arr[i]);
-    }
-}
-
-mapcaoncept((val) => {
-    // console.log(val);
-
-}, arr)
-
-
-// map
-arr.map((val) => console.log(val))
+console.log(b);
