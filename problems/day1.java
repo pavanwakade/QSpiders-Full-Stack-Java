9a6fd208aece5af8c1ae3e4c4ebfa7325ee448a2
pavanwakade <pavanwakade199@gmail.com>
2025-04-21 19:42:11 +0530

    Updated

--- problems/day1.java
+++ problems/day1.java
@@ -29,58 +29,57 @@ Black
 Explanation
 We can clearly see a1 is black in the diagram.*/
 
-
 import java.util.*;
 
 class day1 {
 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
+		Scanner scanner = new Scanner(System.in);
 
-        String input = scanner.next().toLowerCase();
-        char col = input.charAt(0);
-        char row = input.charAt(1);
+		String input = scanner.next().toLowerCase();
+		char col = input.charAt(0);
+		char row = input.charAt(1);
 
-        if (input.length() != 2 || col < 'a' || col > 'h' || row < '1' || row > '8') {
+		if (input.length() != 2 || col < 'a' || col > 'h' || row < '1' || row > '8') {
 
-            System.out.println("invalid input");
-        }
+			System.out.println("invalid input");
+		}
 
-        String BW = "";
-        int hight = 8;
-        char width = 'h';
+		String BW = "";
+		int hight = 8;
+		char width = 'h';
 
-        boolean found = false;
+		boolean found = false;
 
-        for (int i = 1; i <= hight; i++) {
+		for (int i = 1; i <= hight; i++) {
 
-            for (char jj = 'a'; jj <= width; jj++) {
+			for (char jj = 'a'; jj <= width; jj++) {
 
-                BW = jj + "" + "" + i;
-                if (input.equals(BW)) {
+				BW = jj + "" + "" + i;
+				if (input.equals(BW)) {
 //					System.out.println(BW);
-                    found = true;
+					found = true;
 
-                    break;
-                }
-            }
+					break;
+				}
+			}
 
-            if (found) {
-                break;
-            }
+			if (found) {
+				break;
+			}
 
-        }
+		}
 
-        System.out.println();
+		System.out.println();
 
-        int colIndex = col - 'a';
-        int rowIndex = row - '1';
+		int colIndex = col - 'a';
+		int rowIndex = row - '1';
 
-        if ((colIndex + rowIndex) % 2 == 0) {
-            System.out.println("Black");
-        } else {
-            System.out.println("White");
-        }
-    }
+		if ((colIndex + rowIndex) % 2 == 0) {
+			System.out.println("Black");
+		} else {
+			System.out.println("White");
+		}
+	}
 }
