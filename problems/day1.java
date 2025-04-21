c32d28f1362ea6f022ec1c1dda56801a1e2c5e01
pavanwakade <pavanwakade199@gmail.com>
2025-04-21 10:00:15 +0530

    Updated

--- problems/day1.java
+++ problems/day1.java
@@ -7,31 +7,43 @@ import java.util.regex.*;
 class day1 {
 
 	public static void main(String[] args) {
-		int hight = 8;
-		int width = 8;
 
-		for (int i = 1; i <= hight; i++) {
-			
+		Scanner scanner = new Scanner(System.in);
 
-			for (int j = 1; j <= width; j++) {
-//				System.out.println(j);
-				if (j % 2 == 0) {
-					System.out.print(" b"+j);
+		String input = scanner.next().toLowerCase();
+		char col = input.charAt(0);
+		char row = input.charAt(1);
 
-				}
-				else {
-					System.out.print(" w"+j);
-				}
+		if (input.length() != 2 || col < 'a' || col > 'h' || row < '1' || row > '8') {
 
-			}
-			System.out.println();
+			System.out.println("invalid input");
+		}
 
+//		String BW = "";
+//		int hight = 8;
+//		int width = 8;
+//
+//		for (int i = 1; i <= hight; i++) {
+//
+//			for (int j = 1; j <= width; j++) {
+////				System.out.println(j);
+//				if (j % 2 == 0) {
+//
+//					BW = i + " " + " " + j;
+//					
+//					System.out.println(" " + BW);
+//
+//				} else {
+//					System.out.print(" white");
+//				}
+//
+//			}
+//			System.out.println();
+//
+//		}
+		
+		for (char cc = 'a';  cc<= 'z'; cc++) {
+			System.out.println(cc);
 		}
 	}
 }
-
-//    public static swape(String black,String white){
-//        String temp=black;
-//        black=white;
-//        white=temp;
-//    }
