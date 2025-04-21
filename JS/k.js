1596e196fa0fdd49549e25459a35b4959daa2a75
pavanwakade <pavanwakade199@gmail.com>
2025-04-21 13:21:25 +0530

    Updated

---
+++ JS/k.js
@@ -0,0 +1,28 @@
+console.log(1 * "10");
+console.log(10 / 'a');
+console.log(10 + 'a');
+
+let a = Symbol(10)
+let b = Symbol(10)
+console.log(a === b);
+
+let data=[{
+
+    name:"pavan",
+    name:"Ravan"
+},
+{
+
+    name:"pavan"
+},
+{
+
+    name:"pavan"
+},
+{
+
+    name:"pavan"
+}
+]
+
+console.table(data);
--- problems/day1.java
+++ problems/day1.java
@@ -30,60 +30,57 @@ Explanation
 We can clearly see a1 is black in the diagram.*/
 
 
-
-
-
-
-
 import java.util.*;
 
 class day1 {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
 
-		String input = scanner.next().toLowerCase();
-		char col = input.charAt(0);
-		char row = input.charAt(1);
+        String input = scanner.next().toLowerCase();
+        char col = input.charAt(0);
+        char row = input.charAt(1);
 
-		if (input.length() != 2 || col < 'a' || col > 'h' || row < '1' || row > '8') {
+        if (input.length() != 2 || col < 'a' || col > 'h' || row < '1' || row > '8') {
 
-			System.out.println("invalid input");
-		}
+            System.out.println("invalid input");
+        }
 
-		String BW = "";
-		int hight = 8;
-		char width = 'h';
-		
-		boolean found=false;
+        String BW = "";
+        int hight = 8;
+        char width = 'h';
 
-		for (int i = 1; i <= hight; i++) {
+        boolean found = false;
 
-			for (char jj = 'a'; jj <= width; jj++) {
+        for (int i = 1; i <= hight; i++) {
 
-				BW = jj + "" + "" + i;
-				if (input.equals(BW)) {
+            for (char jj = 'a'; jj <= width; jj++) {
+
+                BW = jj + "" + "" + i;
+                if (input.equals(BW)) {
 //					System.out.println(BW);
-					found=true;
+                    found = true;
 
-					break;
-				}
-			}
-			
-			if (found) {
-				break;
-			}
+                    break;
+                }
+            }
 
-		}
+            if (found) {
+                break;
+            }
 
-		 int colIndex = col - 'a';       // e.g. 'a' = 0, 'b' = 1, ...
-	        int rowIndex = row - '1';       // e.g. '1' = 0, '2' = 1, ...
+        }
 
-	        if ((colIndex + rowIndex) % 2 == 0) {
-	            System.out.println("Black");
-	        } else {
-	            System.out.println("White");
-	        }
-	    }
+        System.out.println();
+
+        int colIndex = col - 'a';
+        int rowIndex = row - '1';
+
+        if ((colIndex + rowIndex) % 2 == 0) {
+            System.out.println("Black");
+        } else {
+            System.out.println("White");
+        }
+    }
 }
