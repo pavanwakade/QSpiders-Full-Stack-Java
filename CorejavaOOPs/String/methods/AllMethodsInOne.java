82653695d7097444c497781db0fc7a794bdc8a3f
pavanwakade <pavanwakade199@gmail.com>
2025-04-22 09:21:11 +0530

    Updated

---
+++ CorejavaOOPs/String/methods/AllMethodsInOne.java
@@ -0,0 +1,44 @@
+/**
+ * 
+ */
+package String.methods;
+
+/**
+ * 
+ */
+public class AllMethodsInOne {
+
+	public static void main(String[] args) {
+
+		String s = "Hello good Morning myname is pavan i have my BE in Electronics and Telecommunications";
+		/**
+		 * 1) Length() --> int it print length of String
+		 */
+
+		int length = s.length();
+
+		System.out.println("(Length method) length of string : " + length);
+
+		/**
+		 * 2)charAt(index) -->char it print the value of that index
+		 */
+		char c = s.charAt(0);
+
+		System.out.println("charAt : " + c);
+
+		/**
+		 * 3)codePointAt(index) --> int it print ASCCI value of that index
+		 */
+		int codePointAt = s.codePointAt(0);
+
+		System.out.println("codePointAt : " + codePointAt);
+
+		/**
+		 * 4)codePointBefour(index) -->int
+		 * it ptint the ASCCI value of befour specific index
+		 */
+		int codePointBefour =s.codePointBefore(1);
+		
+		System.out.println("codePointBefore : "+codePointBefour);
+	}
+}
