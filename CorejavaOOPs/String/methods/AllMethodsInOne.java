d701c37d776d1ee04f88e3c369377684914e9eb3
pavanwakade <pavanwakade199@gmail.com>
2025-04-23 19:13:57 +0530

    Updated

--- CorejavaOOPs/String/methods/AllMethodsInOne.java
+++ CorejavaOOPs/String/methods/AllMethodsInOne.java
@@ -10,14 +10,15 @@ public class AllMethodsInOne {
 
 	public static void main(String[] args) {
 
-		String s = "Hello good Morning myname is pavan i have my BE in Electronics and Telecommunications";
+		String s = "Hello 56 good Morning myname is pavan i have my BE in Electronics and Telecommunications";
+		String s1 = "Hello good Morning myname is pavan i have my BE in Electronics and Telecommunications";
 		/**
 		 * 1) Length() --> int it print length of String
 		 */
 
 		int length = s.length();
 
-		System.out.println("(Length method) length of string : " + length);
+		System.out.println("  (Length method) length of string : " + length);
 
 		/**
 		 * 2)charAt(index) -->char it print the value of that index
@@ -34,13 +35,23 @@ public class AllMethodsInOne {
 		System.out.println("codePointAt : " + codePointAt);
 
 		/**
-		 * 4)codePointBefour(index) -->int
-		 * it ptint the ASCCI value of befour specific index
+		 * 4)codePointBefour(index) -->int it ptint the ASCCI value of befour specific
+		 * index
 		 */
-		int codePointBefour =s.codePointBefore(1);
+		int codePointBefour = s.codePointBefore(1);
+
+		System.out.println("codePointBefore : " + codePointBefour);
+
+		/**
+		 * compareTo -->int two string to there ubicode/ASCCIi value it return 0 if
+		 * string are equal else it return another int value if String not equel
+		 */
+		System.out.println(s.compareTo(s1));
 		
-		System.out.println("codePointBefore : "+codePointBefour);
 		
+		String String=s.concat(s1);
 		
+		System.out.println(String);
+
 	}
 }
