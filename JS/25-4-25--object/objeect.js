dbfb2bdb167f87be19fb15582f58f490a2b0a7ac
pavanwakade <pavanwakade199@gmail.com>
2025-04-25 14:20:22 +0530

    Updated

--- JS/25-4-25--object/objeect.js
+++ JS/25-4-25--object/objeect.js
@@ -23,7 +23,7 @@ let person = {
         YOP: 2024,
         collage: {
             name: "SBPCOE",
-            univercity: "SPPU",
+            university: "SPPU",
             address: {
                 city: "indapur",
                 pincode: 411047,
@@ -52,9 +52,89 @@ let person = {
 
 // ?CRUD
 
-person.hobbies.push("BGMI");//!update
+// person.hobbies.push("BGMI");//!update
 
-// delete person.
+// delete person.hobbies;  //!delete
 
-console.log(person); //!read
+// console.log(person); //!read
 
+
+
+let arr = [{
+    id: 1,
+    name: "pavan",
+    yop: 2024
+},
+{
+    id: 2,
+    name: "pavan",
+    yop: 2024
+},
+{
+    id: 3,
+    name: "pavan",
+    yop: 2024
+},
+{
+    id: 4,
+    name: "pavan",
+    yop: 2024
+},
+{
+    id: 5,
+    name: "pavan",
+    yop: 2024
+},
+{
+    id: 6,
+    name: "pavan",
+    yop: 2024
+},
+{
+    id: 7,
+    name: "pavan",
+    yop: 2024
+},
+{
+    id: 8,
+    name: "pavan",
+    yop: 2024
+},
+{
+    id: 9,
+    name: "pavan",
+    yop: 2024
+},
+{
+    id: 10,
+    name: "pavan",
+    yop: 2024
+}]
+
+// arr.map((val) => console.log(val.id));
+// arr.map((val) => console.log(val));
+
+
+let pavan={
+    education:"BE",
+    yop:2024,
+    collage:"SBPCOE",
+    certificates:"Java Full Stack"
+}
+
+
+// console.log("Keys :"+Object.keys(pavan));  //?print all keys
+// console.log("values :"+Object.values(pavan)); //?print all values
+// console.log("entries:"+Object.entries(pavan));//print all
+
+
+
+
+// const objDiv = document.getElementById("obj");
+// arr.map((val) => {
+//     const paragraph = document.createElement("p");
+//     paragraph.textContent = `ID: ${val.id}, 
+//     Name: ${val.name}, 
+//     YOP: ${val.yop}`;
+//     objDiv.appendChild(paragraph);
+// });
