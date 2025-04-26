a05ecf2cd334f1ab7795bdf2a2df1154a8df9447
pavanwakade <pavanwakade199@gmail.com>
2025-04-26 18:01:50 +0530

    Updated

--- JS/26-4-25-this/this.js
+++ JS/26-4-25-this/this.js
@@ -4,7 +4,7 @@ let obj = {
     fun1: () => {
         console.log(this);
     }
-   
+
     ,
     fun2: function () {
         console.log(this);
@@ -15,7 +15,37 @@ let obj = {
     }
 }
 
-obj.fun1();
+// obj.fun1();
 
 
 // callappliedbind
+
+
+let student = {
+    id: 1,
+    name: "pavan",
+    add: "latur"
+}
+let student1 = {
+    id: 1,
+    name: "pavan",
+    add: "latur"
+}
+let studen2t = {
+    id: 1,
+    name: "pavan",
+    add: "latur"
+}
+let student3 = {
+    id: 1,
+    name: "pavan",
+    add: "latur"
+}
+
+
+function check() {
+    console.log(this);
+}
+
+
+check.call(student3)
