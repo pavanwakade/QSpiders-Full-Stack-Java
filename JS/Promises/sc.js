0e1a042edd9995632233f85444a8498a71b60ae3
pavanwakade <pavanwakade199@gmail.com>
2025-05-02 14:51:26 +0530

    Updated

--- JS/Promises/sc.js
+++ JS/Promises/sc.js
@@ -1,28 +1,31 @@
- //promise in js
+//promise in js
 
-      //!js syntax
-      //   let a = 10;
-      //   let promise = new Promise((resolve, reject) => {
-      //     if (a == 10) {
-      //       resolve("a is 10");
-      //     } else {
-      //       reject("a is not 10");
-      //     }
-      //   });
+//!js syntax
+//   let a = 10;
+//   let promise = new Promise((resolve, reject) => {
+//     if (a == 10) {
+//       resolve("a is 10");
+//     } else {
+//       reject("a is not 10");
+//     }
+//   });
 
-      // //   console.log(promise);
+// //   console.log(promise);
 
-      //   promise.then((msg) => console.log(msg));
-      //   promise.catch((msg) => console.log(msg));
-      //   promise.finally((msg) => console.log("end"));
+//   promise.then((msg) => console.log(msg));
+//   promise.catch((msg) => console.log(msg));
+//   promise.finally((msg) => console.log("end"));
 
-      let p = fetch("https://api.github.com/users")
 
-      let p2 = p.then((res) => {
-        return res.json()
-      });
 
-      p2.then((api)=>{
-        console.log(api);
-        
-      })
+let p = fetch("https://api.github.com/users")
+
+let p2 = p.then((res) => {
+    return res.json()
+});
+p2.then((api) => {
+    console.log(api);
+});
+
+
+fetch("https://api.github.com/users").then((res) => res.json()).then((api) => console.log(api))
