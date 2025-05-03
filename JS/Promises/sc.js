79b9c40db3c38ca2af5e57869ac5e178e08951ba
pavanwakade <pavanwakade199@gmail.com>
2025-05-03 13:15:02 +0530

    Updated

--- JS/Promises/sc.js
+++ JS/Promises/sc.js
@@ -1,4 +1,9 @@
 //promise in js
+//promises are the objects that represent the eventual completion 
+// (or failure) of an asynchronous operation and its resulting value.
+//promise is a placeholder for a value that we don't have yet but
+//promise is an object that may produce a single value some time in the future
+
 
 //!js syntax
 //   let a = 10;
@@ -10,7 +15,7 @@
 //     }
 //   });
 
-// //   console.log(promise);
+//   console.log(promise);
 
 //   promise.then((msg) => console.log(msg));
 //   promise.catch((msg) => console.log(msg));
@@ -24,8 +29,12 @@ let p2 = p.then((res) => {
     return res.json()
 });
 p2.then((api) => {
-    console.log(api);
+    // console.log(api);
 });
 
 
-fetch("https://api.github.com/users").then((res) => res.json()).then((api) => console.log(api))
+fetch("https://api.github.com/users").then((res) => res.json()).then((api) => console.log(api)).finally(console.log("hello")
+);
+
+
+
--- JS/jj.java
+++ JS/jj.java
@@ -1,12 +1,12 @@
 class jj {
 
-    String name="John Doe";
-    int age=30;
-    String address="123 Main St";
-    String phone="9999999999";
-    String email="johan@gmail.com";
+    String name = "John Doe";
+    int age = 30;
+    String address = "123 Main St";
+    String phone = "9999999999";
+    String email = "johan@gmail.com";
 
-    //copy constructor
+    // copy constructor
     jj(jj obj) {
         this.name = obj.name;
         this.age = obj.age;
@@ -14,8 +14,10 @@ class jj {
         this.phone = obj.phone;
         this.email = obj.email;
     }
-    //default constructor
-    jj(){}
+
+    // default constructor
+    jj() {
+    }
 
     void display() {
         System.out.println("Name: " + name);
@@ -24,6 +26,7 @@ class jj {
         System.out.println("Phone: " + phone);
         System.out.println("Email: " + email);
     }
+
     public static void main(String[] args) {
 
         // List<String> names = Arrays.asList("John", "Jane", "Jack", "Joe");
@@ -39,15 +42,13 @@ class jj {
         // System.out.println(fruits.get(0)); // Outputs: Banana
 
         // for (String fruit : fruits) {
-        //     System.out.println(fruit);
+        // System.out.println(fruit);
 
         // }
 
-       
-        //copy constructor
+        // copy constructor
         jj obj = new jj();
         obj.display();
         System.out.println("Copying data using default constructor");
-        
     }
 }
