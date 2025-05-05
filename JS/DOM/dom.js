ca921ac20011191df05696d0d636750f2fd54dbf
pavanwakade <pavanwakade199@gmail.com>
2025-05-05 14:18:06 +0530

    Updated

---
+++ JS/DOM/dom.js
@@ -0,0 +1,10 @@
+// console.log(Window.document);
+let heading = document.getElementsByTagName("h1");
+
+for (let i = 0; i < heading.length; i++) {
+    heading[i].style.backgroundColor = "red";
+    
+    
+}
+
+
---
+++ JS/Math.js
@@ -0,0 +1,10 @@
+console.log(Math.round(10.6));  //11
+
+// console.log(Math.random()); //0.5016876268681345    0.9542848404516617
+
+console.log(1000*Math.random()+1000);
+
+let randomm=1000*Math.random()+1000;
+console.log(round(randomm));
+
+
