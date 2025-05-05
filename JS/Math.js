0b23346fcef2eec973bfcef1e87d8b22e85fe7cf
pavanwakade <pavanwakade199@gmail.com>
2025-05-05 15:00:17 +0530

    Updated

--- JS/Math.js
+++ JS/Math.js
@@ -1,10 +1,16 @@
-console.log(Math.round(10.6));  //11
+// console.log(Math.round(10.6));  //11
 
 // console.log(Math.random()); //0.5016876268681345    0.9542848404516617
 
-console.log(1000*Math.random()+1000);
+// console.log(1000*Math.random()+1000);
 
-let randomm=1000*Math.random()+10.;
-console.log(randomm.f);
+// let randomm=1000*Math.random()+1000;
+// console.log(Math.floor(randomm));
 
+
+
+let StringNum=["one","two","three","four","five","six","seven","eight","nine"];
+let num=10;
+
+let aa=num.toString();
 
