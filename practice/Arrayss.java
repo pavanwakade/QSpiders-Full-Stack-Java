fdac916909b46d8b2f0a42fea899cbe93bb8422f
pavanwakade <pavanwakade199@gmail.com>
2025-05-05 22:58:03 +0530

    Updated

--- practice/Arrayss.java
+++ practice/Arrayss.java
@@ -1,9 +1,14 @@
-import java.Util.Arrays;
+import java.util.Arrays;
 class Arrayss{
 public static void main(String[] args) {
 
-int []a;
+int []a,b[],c;
+a=new int [3];
 a[0]=1;
+a[1]=1;
+b=new int[2][3];
 System.out.println(Arrays.toString(a));
+System.out.println(Arrays.toString(b));
+
 }
 }
