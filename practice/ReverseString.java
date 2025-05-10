57be4dc989a378c54dd255f63f9d5c414827750e
pavanwakade <pavanwakade199@gmail.com>
2025-05-10 22:05:24 +0530

    Updated

--- practice/ReverseString.java
+++ practice/ReverseString.java
@@ -1,16 +1,15 @@
 class ReverseString{
 
 	public static void main(String[] args) {
-		reverse("paban");
+	System.out.println(reverse("paban"));
 	}
 	public static String reverse(String str){
 		char []ch=str.toCharArray();
 		String s="";
 		int j=ch.length-1;
-		for (int i=0;i<ch.length-1 ;i++,j-- ) {
+		for (int i=0;i<ch.length;i++,j-- ) {
 			s+=ch[j];
 			// System.out.println(s);
 		}
 		return s;
 	}
-}
