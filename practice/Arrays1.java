224cbd404735b9f145662fdd05fb2c11730674be
pavanwakade <pavanwakade199@gmail.com>
2025-05-10 18:38:10 +0530

    Updated

--- practice/Arrays1.java
+++ practice/Arrays1.java
@@ -2,16 +2,18 @@ import java.util.Arrays;
 class Arrays1{
 	public static void main(String[] args) {
 		
+		System.out.println(checkpalindrom("pavan"));
+	}
 
-		String str="ama";
+	public static boolean checkpalindrom(String str){
 		char []c=str.toCharArray();
 		int j=c.length-1;
 		for (int i=0;i<c.length ;i++,j-- ) {
 			if (c[i]!=c[j]) {
 				System.out.println("not palindrom");
-				break;
+				return false;
 			}
 		}
-		System.out.println("palindrom");
+		return true;
 	}
 }
