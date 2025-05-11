5401df75168f7af8a2b5e402e69ffae1fb587883
pavanwakade <pavanwakade199@gmail.com>
2025-05-11 15:43:41 +0530

    Updated

--- problems/problems/findingDuplicateinString.java
+++ problems/problems/findingDuplicateinString.java
@@ -1,18 +1,19 @@
 class findingDuplicateinString{
 
 	public static void main(String[] args) {
-		findingDuplicate("hello");
+		int []a={1,2,3,4,5,7,7,8,};
+		findingDuplicate(a);
 		
 	}
 
-	public static void findingDuplicate(String str){
-		char []c=str.toCharArray();
+	public static void findingDuplicate(int c[]){
+		// char []c=str.toCharArray();
         int count=0;
 		for (int i=0;i<c.length ;i++ ) {
-			for(int j=i;j<c.length;j++){
-				if(c[i]==c[j]){
+			for(int j=i;j<c.length-1;j++){
+				// if(c[i]==c[j]){
 			System.out.println(c[i]+" " +c[j]);
-				}
+				// }
 			}
 		}
 		// System.out.println(count);
