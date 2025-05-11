2af50bdea4ac82c10a61986db4a462ef72b2000e
pavanwakade <pavanwakade199@gmail.com>
2025-05-11 14:52:35 +0530

    Updated

--- problems/problems/findingDuplicateinString.java
+++ problems/problems/findingDuplicateinString.java
@@ -1,22 +1,31 @@
-class findingDuplicateinString{
+class findingDuplicateinString {
+    public static void main(String[] args) {
+        findingDuplicate("hello");
+    }
 
-	public static void main(String[] args) {
-		findingDuplicate("hello");
-		
-	}
-
-	public static void findingDuplicate(String str){
-		char []c=str.toCharArray();
-        int count=0;
-		for (int i=0;i<c.length ;i++ ) {
-			for (int j=i;j<c.length ;j++ ) {
-				if (c[i]==c[j]) {
-					count++;
-				}
-			}
-			
-		}
-		System.out.println(count);
-	}
-
+    public static void findingDuplicate(String str) {
+        char[] c = str.toCharArray();
+        int duplicateCount = 0;
+        // Array to mark characters as counted
+        boolean[] counted = new boolean[c.length];
+        
+        for (int i = 0; i < c.length; i++) {
+            // Skip if already counted as a duplicate
+            if (counted[i]) continue;
+            
+            int charCount = 0;
+            for (int j = i; j < c.length; j++) {
+                if (c[i] == c[j]) {
+                    charCount++;
+                    if (j != i) counted[j] = true; // Mark duplicates as counted
+                }
+            }
+            // If the character appears more than once, increment duplicateCount
+            if (charCount > 1) {
+                duplicateCount++;
+                System.out.println("Duplicate found: '" + c[i] + "' appears " + charCount + " times");
+            }
+        }
+        System.out.println("Total number of duplicate characters: " + duplicateCount);
+    }
 }
