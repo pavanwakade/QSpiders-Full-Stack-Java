cf7f35e23eceb51307aa66b90a8a463e53f936fd
pavanwakade <pavanwakade199@gmail.com>
2025-05-11 13:13:40 +0530

    Updated

--- problems/problems/reverseString.java
+++ problems/problems/reverseString.java
@@ -1,8 +1,24 @@
+import java.util.Arrays;
 class reverseString{
 	public static void main(String[] args) {
-      // System.out.println("hello");
+      System.out.println(reverse("pavan"));
 
   }
 
-  
+  public static String reverse(String str){
+  	char []a=str.toCharArray();
+
+    // System.out.println(Arrays.toString(a));
+
+    String s="";
+
+  	int i=a.lenght-1;
+    System.out.println(i);
+
+    for (int j = 0; j < a.length; j++, i--){
+  		 s+=a[i];
+  	}
+  	// System.out.println(s);
+    return s;
+  }
 }
