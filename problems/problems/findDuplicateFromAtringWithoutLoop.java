a0f5959870dbb8064ee796af0e3f9f63da53358e
pavanwakade <pavanwakade199@gmail.com>
2025-05-11 14:22:22 +0530

    Updated

--- problems/problems/findDuplicateFromAtringWithoutLoop.java
+++ problems/problems/findDuplicateFromAtringWithoutLoop.java
@@ -1,1 +1,10 @@
-findDuplicateFromAtringWithoutLoop.java
+class findDuplicateFromAtringWithoutLoop{
+	public static void main(String[] args) {
+		
+	}
+
+	public static void duplicate(String str){
+		char c=str.toCharArray();
+		
+	}
+}
--- problems/problems/findingDuplicateinString.java
+++ problems/problems/findingDuplicateinString.java
@@ -1,1 +1,22 @@
-findingDuplicateinString.java
+class findingDuplicateinString{
+
+	public static void main(String[] args) {
+		findingDuplicate("hello");
+		
+	}
+
+	public static void findingDuplicate(String str){
+		char []c=str.toCharArray();
+        int count=0;
+		for (int i=0;i<c.length ;i++ ) {
+			for (int j=i;j<c.length ;j++ ) {
+				if (c[i]==c[j]) {
+					count++;
+				}
+			}
+			
+		}
+		System.out.println(count);
+	}
+
+}
