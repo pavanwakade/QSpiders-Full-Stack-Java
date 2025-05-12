9a1e4ac779054fd40c1065229c99eb7b37c67af9
pavanwakade <pavanwakade199@gmail.com>
2025-05-12 12:42:49 +0530

    Updated

---
+++ problems/problems/BouncyArray.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+class bauncyArray{
+public static void main(String[] args) {
+	int []a={1,2,3,4,5};
+	int []b={10,20,30,40,50};
+System.out.println(Arrays.toString(bouncy(a,b)));
+}
+
+public static int[] bouncy(int a[],int b[]){
+	int []c=new int[a.length+b.length];
+int j=0;
+	for (int i=0;c.length;i++) {
+		if (i<a.length&&i<b.length) {
+			c[j++]=a[i];
+		    c[j++]=b[i];
+		}
+		if(i<a.length){
+       c[j++]=a[i];
+	}
+
+	if(i<b.length){
+      c[j++]=a[i];
+	}
+	}
+	return c;
+}
+}
