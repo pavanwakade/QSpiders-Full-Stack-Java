c3a92c2a80b1b5b53392915e3e77f85a0bdf87c3
pavanwakade <pavanwakade199@gmail.com>
2025-05-16 12:39:31 +0530

    Updated

---
+++ problems/problems/prb2.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package problems;
+
+import java.util.LinkedHashMap;
+
+/**
+ * 
+ */
+public class prb2 {
+	
+	public static void main(String[] args) {
+		int []a= {1,3,3,5,3,5};
+		
+		
+	}
+
+}
