00d840dad9b1aed87c43342a38807c3319ef01a1
pavanwakade <pavanwakade199@gmail.com>
2025-05-16 13:11:17 +0530

    Updated

--- problems/problems/prb2.java
+++ problems/problems/prb2.java
@@ -1,19 +1,29 @@
-/**
- * 
- */
 package problems;
 
-import java.util.LinkedHashMap;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
 
-/**
- * 
- */
 public class prb2 {
-	
-	public static void main(String[] args) {
-		int []a= {1,3,3,5,3,5};
-		
-		
-	}
 
+	public static void main(String[] args) {
+		int[] a = { 1, 3, 3, 5, 3, 5 };
+		Integer bb=a;
+
+		int count = 0;
+//		List<Integer> list = new ArrayList<Integer>(Arrays.asList(a));
+		Set<Integer> treeSet = new TreeSet<Integer>();
+
+		// Add elements from array to the TreeSet
+		for (int value : a) {
+			treeSet.add(value);
+		}
+
+		for (Integer integer : treeSet) {
+			count++;
+		}
+		System.out.println(count);
+	}
 }
