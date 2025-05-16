7ecb27aa9db1597f027fad7d13aa310c2e1fa070
pavanwakade <pavanwakade199@gmail.com>
2025-05-16 21:00:28 +0530

    Updated

--- problems/problems/prb2.java
+++ problems/problems/prb2.java
@@ -1,8 +1,5 @@
 package problems;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 import java.util.Set;
 import java.util.TreeSet;
 
@@ -10,8 +7,6 @@ public class prb2 {
 
 	public static void main(String[] args) {
 		int[] a = { 1, 3, 3, 5, 3, 5 };
-		Integer bb=a;
-
 		int count = 0;
 //		List<Integer> list = new ArrayList<Integer>(Arrays.asList(a));
 		Set<Integer> treeSet = new TreeSet<Integer>();
