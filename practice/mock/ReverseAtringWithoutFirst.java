5af8cb9dc552b45259b930b2df439d093b1c9f2e
pavanwakade <pavanwakade199@gmail.com>
2025-05-18 00:03:32 +0530

    Updated

--- practice/mock/ReverseAtringWithoutFirst.java
+++ practice/mock/ReverseAtringWithoutFirst.java
@@ -1,1 +1,18 @@
-ReverseAtringWithoutFirst
+class ReverseAtringWithoutFirst{
+
+	public static void main(String[] args) {
+		
+		String str="i love coading";
+
+		String newChar=str.split("");
+        char ch[]=new char[newChar.length];
+		for (int i=0;i<newChar.length ;i++ ) {
+
+			ch[i]=newChar[newChar.length-1-i];
+
+		}
+
+		String ss=new String(ch);
+		System.out.println(ss);
+	}
+}
