886e5a69fbcaa2cf891194643a8549b892922860
pavanwakade <pavanwakade199@gmail.com>
2025-05-17 14:50:00 +0530

    Updated

--- practice/Countsries.java
+++ practice/Countsries.java
@@ -1,27 +1,35 @@
 import java.util.ArrayList;
+import java.util.Set;
+import java.util.TreeSet;
 class Countsries{
 	public static void main(String[] args) {
 
 		int a[]={1,3,3,4,5};
-	// 	int count=0;
-	// 	for (int i=0;i<a.length ;i++ ) {
-	// 		boolean isDuplicate=false;
+		int count=0;
+		for (int i=0;i<a.length ;i++ ) {
+			boolean isDuplicate=false;
 
-	// 		for (int j=i+1;j<a.length ;j++ ) {				
-	// 			if (a[i]==a[j]) {
-	// 				isDuplicate=true;
-	// 				break;
-	// 			}
-	// 		}
-	// 		if (!isDuplicate) {
-	// 			count++;
-	// 		}
-	// 	}
-	// 	System.out.println(count);
-	// }
-
-		Set st=new TreeSet();
-		for (int a :st ) {
-			st.add(a);
+			for (int j=i+1;j<a.length ;j++ ) {				
+				if (a[i]==a[j]) {
+					isDuplicate=true;
+					break;
+				}
+			}
+			if (!isDuplicate) {
+				count++;
+			}
 		}
+		System.out.println(count);
+
+
+
+
+		Set<Integer> st=new TreeSet<>();
+		for (int ii :a ) {
+			st.add(ii);
+		}
+
+		System.out.println(st.size());
+
+	}
 }
