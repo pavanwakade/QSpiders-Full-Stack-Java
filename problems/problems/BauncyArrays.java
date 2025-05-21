365ef35c82b61dd4b67b46a9d93ec1e0650d31d2
pavanwakade <pavanwakade199@gmail.com>
2025-05-21 06:58:46 +0530

    Updated

--- problems/problems/BauncyArrays.java
+++ problems/problems/BauncyArrays.java
@@ -7,24 +7,37 @@ public class BauncyArrays {
 		int[] a = { 1, 2, 3, 4, 5 };
 		int[] b = { 10, 20, 30, 40, 50, 60, 70 };
 		System.out.println(Arrays.toString(bouncy(a, b)));
+		mearg(a, b);
 	}
 
 	public static int[] bouncy(int a[], int b[]) {
-		int[] c = new int[a.length + b.length];    // 12
+		int[] c = new int[a.length + b.length]; // 12
 		int j = 0;
 		for (int i = 0; i < c.length; i++) {
 			if (i < a.length && i < b.length) {
 				c[j++] = a[i];
 				c[j++] = b[i];
-			}
-			else if (i < a.length) {
+			} else if (i < a.length) {
 				c[j++] = a[i];
-			}
-
-			else if (i < b.length) {
+			} else if (i < b.length) {
 				c[j++] = b[i];
 			}
 		}
 		return c;
+	}
+
+	
+	
+	public static void mearg(int a[], int b[]) {
+		int[] c = new int[a.length + b.length]; // 12
+		int j = 0;
+		for (int i = 0; i < c.length; i++) {
+		 if (i <= a.length) {
+				c[j] = a[i];
+			} else if (i < b.length) {
+				c[j] = b[i];
+			}
+		}
+		System.out.println(Arrays.toString(c));
 	}
 }
