d32586be8f805a0b289bc008a024a75fcd6adb03
pavanwakade <pavanwakade199@gmail.com>
2025-05-23 09:15:30 +0530

    Updated

--- practice/BubbleSort.java
+++ practice/BubbleSort.java
@@ -1,28 +1,40 @@
 import java.util.Arrays;
+import java.util.List;
+import java.util.ArrayList;
+
+import java.util.Collections;
 class BubbleSort{
+
+
+public static  void bubblesort(int a[]){
+		for (int i=0;i<a.length;i++ ) {
+			for (int j=0;j<a.length-1-i ;j++ ) {
+				if (a[j]>a[j+1]) {
+					int temp=a[j];
+					a[j]=a[j+1];
+					a[j+1]=temp;
+				}
+			}
+		}
+	}
+
 	public static void main(String[] args) {
-		int [] a={1,4,5,6};
+		int [] a={1,4,5,6,9,2,5,3,0};
 		System.out.println(Arrays.toString(a));
 		bubblesort(a);
 	    System.out.println(Arrays.toString(a));
 
-	}
 
-	// public Bubblesort(){}
 
-	public static  void bubblesort(int a[]){
+	    // Arrays.sort(a);
+	    // System.out.println(Arrays.toString(a));
 
-		for (int i=0;i<a.length-1 ;i++ ) {
-			for (int j=0;j<a.length-1-i ;j++ ) {
-				// System.out.print("a "+a[i]+"  ");
-				// System.out.print("b  "+a[j]+"  ");
 
-				if (a[i]>a[j]) {
-					int temp=a[i];
-					a[i]=a[j];
-					a[j]=temp;
-				}
-			}
-		}
+	    // List<Integer> list=new ArrayList<>();
+	    // for (int i :a ) {
+	    // 	list.add(i);
+	    // }
+	    // Collections.sort(list);
+	    // System.out.println(list);
 	}
 }
