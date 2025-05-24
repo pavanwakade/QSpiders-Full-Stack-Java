49b722d0c7d0f5b42d15e98fd7e83a37e43e378f
pavanwakade <pavanwakade199@gmail.com>
2025-05-24 09:05:38 +0530

    Updated

---
+++ CorejavaOOPs/collection/CollectionMethodes.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package collection;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * 
+ */
+public class CollectionMethodes {
+
+	
+	
+	Collections collections=new Collections(List.of(1,2,3,4)); 
+	
+	public static void main(String[] args) {
+		
+	}
+}
