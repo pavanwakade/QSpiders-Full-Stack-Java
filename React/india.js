b87268811308845b01cd786039ee59d07f9a7a94
pavanwakade <pavanwakade199@gmail.com>
2025-06-03 12:05:08 +0530

    Updated

--- React/india.js
+++ React/india.js
@@ -2,5 +2,14 @@
 
 // export default phone;
 
-let resourses="crud oil";
-export default resourses
+
+
+let resourses = "crud oil";
+// export default resourses
+
+
+let resourses2 = "wepons";
+
+// export default { 
+//     resourses,
+//      resourses2 }
--- React/russia.js
+++ React/russia.js
@@ -7,5 +7,13 @@
 // console.log(resourses);
 
 
+// import indianoil from "./india.js"; //!we can import with diffrant name  with default export
+// console.log(indianoil);
+
+
 import indianoil from "./india.js"; //!we can import with diffrant name  with default export
-console.log(indianoil);
+// console.log(indianoil);
+let {resourses,resourses2}=indianoil;   //!import and destucture for easy access
+
+console.log(resourses);
+console.log(resourses2);
