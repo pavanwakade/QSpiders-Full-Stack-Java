a7a81a700f836443f48087b15d1d9b32771ee35a
pavanwakade <pavanwakade199@gmail.com>
2025-06-10 23:24:41 +0530

    Updated

---
+++ React/firstTest/src/App.jsx
@@ -0,0 +1,19 @@
+import { Component } from 'react';
+import './App.css';
+import Components from './Components/';
+class App extends Component {
+ 
+  render(){
+    return(
+        <div className='App'>
+          <Components />
+        </div>
+
+    );
+  }
+}
+export default App;
+
+
+
+
---
+++ React/firstTest/src/Components/Components.jsx
@@ -0,0 +1,5 @@
+import { Component } from "react";
+
+class Components extends Compon {
+   
+}
