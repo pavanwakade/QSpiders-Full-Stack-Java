d06400cdef27c64347036aab9c4f1c0422572bf7
pavanwakade <pavanwakade199@gmail.com>
2025-06-13 15:05:08 +0530

    Updated

--- CorejavaOOPs/collectionFramework/collectioninterface/CollectionInterfaceMethods.java
+++ CorejavaOOPs/collectionFramework/collectioninterface/CollectionInterfaceMethods.java
@@ -3,6 +3,8 @@
  */
 package collectionFramework.collectioninterface;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 /**
@@ -11,11 +13,35 @@ import java.util.List;
 public class CollectionInterfaceMethods {
 
 	public static void main(String[] args) {
-		
-		List<E>
-		
+
+		Collection<Object> coll = new ArrayList<>();
+
+		// add
+
+		coll.add("String");
+		coll.add(1);
+		coll.add(true);
+		coll.add(1.1);
+		coll.add(null);
+//		System.out.println("Add()  "+coll);
+
+		Collection<String> coll1 = new ArrayList<>();
+		coll1.add("String");
+		coll1.add("pavan");
+		coll1.add("savan");
+		coll1.add("jivan");
+
+		System.out.println("orignal " + coll1);
+		// remove using element
+		coll1.remove("String");
+
+		System.out.println("remove()" + coll1);
+
+		// remove using index
+		((List<String>) coll1).remove(1);
+
+		System.out.println("remove()" + coll1);
+
 	}
-	
-	
-	
+
 }
