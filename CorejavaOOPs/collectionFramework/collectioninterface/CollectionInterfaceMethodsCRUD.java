55d5cbf68a29074906a7a0418cdc7900c98bbc53
pavanwakade <pavanwakade199@gmail.com>
2025-06-13 16:55:29 +0530

    Updated

--- CorejavaOOPs/collectionFramework/collectioninterface/CollectionInterfaceMethodsCRUD.java
+++ CorejavaOOPs/collectionFramework/collectioninterface/CollectionInterfaceMethodsCRUD.java
@@ -1,108 +1,107 @@
 package collectionFramework.collectioninterface;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Iterator;
-import java.util.List;
-import java.util.ListIterator;
-import java.util.Scanner;
+import java.util.*;
 
 public class CollectionInterfaceMethodsCRUD {
 
-	static Collection<String> coll = new ArrayList<String>();
+	static Collection<String> coll = new ArrayList<>();
 	static Scanner sc = new Scanner(System.in);
-	static boolean opt=true;
+	static boolean opt = true;
+
 	public static void main(String[] args) {
-//		addINtoCollection();
-		
 		while (opt) {
 			choice();
 		}
 	}
 
 	public static void choice() {
+		System.out.println("\nChoose an option:");
+		System.out.println("1. Insert");
+		System.out.println("2. Read");
+		System.out.println("3. Update");
+		System.out.println("4. Delete");
+		System.out.println("5. Stop");
+		System.out.print("Your choice: ");
 
-		System.out.println("Enter currect option");
-		System.out.println("1 read \n 2 insert \n 3 update /n 4 delete \n 5 stop \n currect option");
 		int inp = sc.nextInt();
+		sc.nextLine(); 
 
 		switch (inp) {
 		case 1: {
-			System.out.println(coll);
-			break;
-		}
-		case 2: {
 			addINtoCollection();
 			break;
 		}
-
+		case 2: {
+			System.out.println("Data: " + coll);
+			break;
+		}
 		case 3: {
-			System.out.println("Enter old value which you need to change");
-			String oldvalue = sc.nextLine();
-
-			System.out.println("Enter new value");
+			System.out.print("Enter old value to update: ");
+			String oldValue = sc.nextLine();
+			System.out.print("Enter new value: ");
 			String newValue = sc.nextLine();
-			UpdateCollection(coll, oldvalue, newValue);
+			UpdateCollection(coll, oldValue, newValue);
 			break;
 		}
 		case 4: {
-			System.out.println("Enter  value which you need to delete");
-			String oldvalue = sc.nextLine();
-			RemoveFromCollection(coll, oldvalue);
+			System.out.print("Enter value to delete: ");
+			String value = sc.nextLine();
+			RemoveFromCollection(coll, value);
 			break;
 		}
 		case 5: {
-			System.out.println("stoping operations .......");
-			opt=false;
+			System.out.println("Stopping operations...");
+			opt = false;
 			break;
 		}
 		default: {
-
 			System.err.println("Unexpected value: " + inp);
+			break;
 		}
 		}
-
 	}
 
 	public static Collection<String> addINtoCollection() {
-		for (int i = 0; i <= 3; i++) {
-			sc.next();
-			System.out.println("entr String");
+		for (int i = 0; i < 3; i++) {
+			System.out.print("Enter String: ");
 			String obj = sc.nextLine();
 			coll.add(obj);
 		}
-//		System.out.println("insert   :" + coll);
 		return coll;
 	}
 
 	public static Collection<String> RemoveFromCollection(Collection<String> str, String ss) {
-
 		Iterator<String> it = str.iterator();
-
+		boolean found = false;
 		while (it.hasNext()) {
 			if (it.next().equalsIgnoreCase(ss)) {
-
 				it.remove();
+				found = true;
 			}
 		}
-		System.out.println("Remove   :" + coll);
-		return coll;
+		if (found)
+			System.out.println("Removed: " + ss);
+		else
+			System.out.println("Value not found to remove.");
+		return str;
 	}
 
 	public static Collection<String> UpdateCollection(Collection<String> str, String ss, String nn) {
-
 		List<String> list = (List<String>) str;
 		ListIterator<String> it = list.listIterator();
+		boolean updated = false;
 
 		while (it.hasNext()) {
 			if (it.next().equalsIgnoreCase(ss)) {
-
 				it.set(nn);
+				updated = true;
 			}
 		}
-		System.out.println("updated  :" + coll);
 
-		return coll;
-
+		if (updated)
+			System.out.println("Updated '" + ss + "' to '" + nn + "'");
+		else
+			System.out.println("Value not found to update.");
+		return str;
 	}
 }
