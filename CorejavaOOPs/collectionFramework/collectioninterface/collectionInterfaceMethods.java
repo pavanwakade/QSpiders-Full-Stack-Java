5527d3c07c66da153a38c7a886b5698aeaeec76a
pavanwakade <pavanwakade199@gmail.com>
2025-06-13 09:25:32 +0530

    Updated

--- CorejavaOOPs/collectionFramework/collectioninterface/collectionInterfaceMethods.java
+++ CorejavaOOPs/collectionFramework/collectioninterface/collectionInterfaceMethods.java
@@ -1,29 +1,37 @@
 package collectionFramework.collectioninterface;
+
 import java.util.Collection;
 import java.util.Arrays;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+public class CollectionInterfaceMethods {
 
-public class collectionInterfaceMethods {
-
-     static  Collection<String> coll=new ArrayList();
-     static  Scanner sc=new Scanner(System.in);
+	static Collection<String> coll = new ArrayList<String>();
+	static Scanner sc = new Scanner(System.in);
 
 	public static void main(String[] args) {
 		addINtoCollection();
+		RemoveFromCollection(coll,"java");
 	}
 
-	public static void addINtoCollection(){
-		for (int i=0;i<=5;i++ ) {
+	public static Object addINtoCollection() {
+		for (int i = 0; i <= 5; i++) {
 			System.out.println("entr String");
-			String obj=sc.nextLine();
-		    coll.add(obj);
+			String obj = sc.nextLine();
+			coll.add(obj);
+		}
+		return coll;
 	}
-	System.out.println(coll);
 
+	public static Object RemoveFromCollection(Collection<String> str,String ss) {
+		
+		for (String string : str) {
+			if (str.contains(ss)) {
+				str.remove(ss);
+			}
+		}
+		return str;
+		}
 	}
 
-
-
-}
