888747942a5edace82eeda962cb6677317028139
pavanwakade <pavanwakade199@gmail.com>
2025-06-13 11:55:31 +0530

    Updated

--- CorejavaOOPs/collectionFramework/collectioninterface/Collection1.java
+++ CorejavaOOPs/collectionFramework/collectioninterface/Collection1.java
@@ -1,41 +1,50 @@
 package collectionFramework.collectioninterface;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
-import java.util.Collection;
-import java.util.HashSet;
-import java.util.TreeSet;
+import java.util.Arrays;
+import java.util.List;
+
 
 public class Collection1 {
+    public static void main(String[] args) {
+        List<String> names = new ArrayList<>();
+        names.add("Alice");
+        names.add("Bob");
+        names.add("Charlie");
+        names.add("Charlie");
+        names.add("Charlie");
 
-//	@SuppressWarnings("unchecked")
-	public static void main(String[] args) {
+        String[] nameObjects = names.toArray(new String[0]);
+        int len=nameObjects.length;
+        
+        for (int i = 0; i < len; i++) {
+			System.out.println(nameObjects[i]);
+		}
 
-		Collection col = new ArrayList<>(); // ArrayList: When you need fast access by index
-		col.add(1);
-		col.add(2);
-		col.add(2);
-		col.add(6);
-		col.add(6);
-		col.add(1);
-		System.out.println(col); // printed same value [1, 2, 6, 1]
-
-		Collection tree = new TreeSet<>(); // TreeSet: When you need elements to stay sorted
-		tree.add(1);
-		tree.add(6);
-		tree.add(6);
-		tree.add(1);
-		tree.add(2);
-		tree.add(2);
-		System.out.println(tree); // printed unique value [1, 2, 6]
-
-		Collection set = new HashSet<>(); // HashSet: When you need fast lookups and don't care about order
-		set.add(1);
-		set.add(2);
-		set.add(2);
-		set.add(6);
-		set.add(6);
-		set.add(1);
-		System.out.println(set); // printed unique value [1, 2, 6]
-
-	}
+        System.out.println(Arrays.toString(nameObjects));
+        
+        List<Object>kkList=new ArrayList<>();
+        kkList.add(1);
+        kkList.add("pavan");
+        kkList.add(4.4);
+        kkList.add(true);
+        kkList.add(1);
+        
+        Object[] ssObjects=kkList.toArray();
+        
+        for (int i = 0; i < ssObjects.length; i++) {
+        	
+			if ( ssObjects[i] instanceof Integer) {
+				
+				System.out.println(ssObjects[i]);
+			}
+		}
+        
+        System.out.println(Arrays.toString(ssObjects));
+        
+        // What if we try to store it as String[] directly?
+        // String[] nameStrings = (String[]) names.toArray(); // This would compile but throw ClassCastException at runtime!
+                                                           // An Object[] cannot be cast to a String[]
+    }
 }
