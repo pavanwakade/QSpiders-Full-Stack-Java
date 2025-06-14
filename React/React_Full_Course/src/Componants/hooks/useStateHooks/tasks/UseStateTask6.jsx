d874607aa8efef3cfb6adab8f08a49f570494008
pavanwakade <pavanwakade199@gmail.com>
2025-06-14 14:34:54 +0530

    Updated

--- React/React_Full_Course/src/Componants/hooks/useStateHooks/tasks/UseStateTask6.jsx
+++ React/React_Full_Course/src/Componants/hooks/useStateHooks/tasks/UseStateTask6.jsx
@@ -1,9 +1,9 @@
 let UseStateTask6=()=>{
     return(
-        <div>
-            <h1>hello</h1>
+        <div style={{display:"flex",justifyContent:"center",alignContent:"center", height:"100vh",width:"100"}}>
+            {/* <h1>hello</h1> */}
             <div style={{
-                width:"50%",
+                width:"50px",
                 height:"50px",
                 borderRadius:"50%",
                 border:"1px solid black"
