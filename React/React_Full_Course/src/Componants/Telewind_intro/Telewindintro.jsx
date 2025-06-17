62d79298fad534f48da73d3f83ed8f5c350df545
pavanwakade <pavanwakade199@gmail.com>
2025-06-17 16:58:11 +0530

    Updated

--- React/React_Full_Course/src/Componants/Telewind_intro/Telewindintro.jsx
+++ React/React_Full_Course/src/Componants/Telewind_intro/Telewindintro.jsx
@@ -4,23 +4,20 @@ let Telewindintro = () => {
         // <div className='Text-lg'>
         //     <h1>hello from Tailwind</h1>
         // </div>
+<div className="bg-[#353434] h-[50px] text-white flex items-center justify-between px-10">
+  {/* Left Section - Logo */}
+  <div className="text-[25px] font-semibold">
+    <a href="#">Logo</a>
+  </div>
 
-        <div>
-            <div className="flex items-center bg-[#353434] text-justify color h-[50px] text-white">
+  {/* Right Section - Navigation Links */}
+  <div className="flex space-x-8 text-[18px]">
+    <a href="#" className="hover:underline decoration-[green] decoration-2">Home</a>
+    <a href="#" className="hover:underline">About</a>
+    <a href="#" className="hover:underline">Career</a>
+  </div>
+</div>
 
-                <div className="flex items-start">
-                <a href=" " className="p-10"> logo</a>
-                </div>
-                <div>
-
-                <a href="" className="p-10">Home </a>
-                <a href=" " className="p-10"> about</a>
-                <a href="" className="p-10">carrier </a>
-                </div>
-
-                
-            </div> 
-        </div>
     )
 
 }
