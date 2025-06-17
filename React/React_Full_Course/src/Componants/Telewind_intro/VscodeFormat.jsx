6ca922c6f233f1cb4e21e667d01b3f6fc81d077c
pavanwakade <pavanwakade199@gmail.com>
2025-06-17 17:58:11 +0530

    Updated

--- React/React_Full_Course/src/Componants/Telewind_intro/VscodeFormat.jsx
+++ React/React_Full_Course/src/Componants/Telewind_intro/VscodeFormat.jsx
@@ -1,17 +1,26 @@
-let VscodeFormat =()=>{
-return(
-    <div className="h-[100vh] w-[100%] bg-[#0f0f0fc5]">
+let VscodeFormat = () => {
+    return (
+        <div className="h-[100vh] w-[100%] bg-[#0f0f0fd5] ">
 
-<div className="navbar  ">
-    <a href="">logo</a>
-    <a href="">File</a>
-    <a href="">logo</a>
-</div>
+            <div className="navbar h-[40px]  text-[white]  bg-[#5a5a5a] flex ">
+                <div className="span1 flex h-full w-[30%] text-start justify-around items-center">
+                    <a href="">logo</a>
+                    <a href="">File</a>
+                    <a href="">Selection</a>
+                    <a href="">View</a>
+                    <a href="">Go</a>
+                    <a href="">Run</a>
+                </div>
+                <div className="span2 bg-[#dbcfcfc5] flex h-full w-[30%] ">
+<a href=""></a>
+                </div>
+                <div className="span3"></div>
+            </div>
 
-<div></div>
+            <div></div>
 
- </div>
-)
+        </div>
+    )
 }
 
 export default VscodeFormat;
