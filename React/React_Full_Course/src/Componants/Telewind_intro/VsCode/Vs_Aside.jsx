b4627dd8402d7f321e1b3b0139aee690b3eeaa30
pavanwakade <pavanwakade199@gmail.com>
2025-06-18 11:12:34 +0530

    Updated

--- React/React_Full_Course/src/Componants/Telewind_intro/VsCode/Vs_Aside.jsx
+++ React/React_Full_Course/src/Componants/Telewind_intro/VsCode/Vs_Aside.jsx
@@ -9,20 +9,17 @@ import Files from './Files';
 let Vs_Aside = () => {
 
   
-  let [active, setActive] = useState(false);
+//   let [active, setActive] = useState(false);
 
-  let Asd =()=>{
-  
-  switch (active) {
-    case "Files": return <Files />
-  }
-}
+//   let Asd =()=>{
+//   switch (active) {
+//     case "Files": return <Files />
+//   }
+// }
   
   return (
     <div className="flex flex-col items-center gap-y-4  bg-[#5a5a5a]  w-[4%] h-[100%] z-0 fixed top-10 left-0">
-      <button onClick={() => {
-        setActive(!active);
-      }}><img src={filepng} alt="File" className="h-[25px] m-[10px]" title='files' /></button>
+      <button onClick={() => {  }}><img src={filepng} alt="File" className="h-[25px] m-[10px]" title='files' /></button>
       <button onClick={() => {}}><img src={searchs} alt="search" className="h-[25px] m-[10px]" title='search' /></button>
       <button onClick={() => {}}><img src={sourse} alt="sourse" className="h-[25px] m-[10px]" title='sourse' /></button>
       <button onClick={() => {}}><img src={debug} alt="debug" className="h-[25px] m-[10px]" title='debug' /></button>
@@ -30,5 +27,4 @@ let Vs_Aside = () => {
     </div>
   );
 };
-
 export default Vs_Aside;
