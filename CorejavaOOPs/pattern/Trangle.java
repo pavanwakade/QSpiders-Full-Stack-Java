25d5b776aa8a3343b75a0595b8c976e5fd7f73a6
pavanwakade <pavanwakade199@gmail.com>
2025-06-18 21:37:31 +0530

    Updated

---
+++ CorejavaOOPs/pattern/Trangle.java
@@ -0,0 +1,16 @@
+package pattern;
+import java.util.Scanner;
+class Trangle{
+	public static void main(String[] args) {
+		
+		Scanner sc= new Scanner(System.in);
+
+		int in=sc.nextInt();
+
+		for (int i=0;i<in ;i++ ) {
+
+			System.out.print("*");
+			
+		}
+	}
+}
