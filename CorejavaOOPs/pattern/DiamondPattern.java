aecdfab5430c663c9f6209fa50a7e4e900f1fb97
pavanwakade <pavanwakade199@gmail.com>
2025-06-18 23:07:26 +0530

    Updated

--- CorejavaOOPs/pattern/DiamondPattern.java
+++ CorejavaOOPs/pattern/DiamondPattern.java
@@ -2,37 +2,69 @@ import java.util.Scanner;
 
 public class DiamondPattern {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+       Scanner sc= new Scanner(System.in);
 
-        System.out.print("Enter number of rows (half of diamond height): ");
-        int n = sc.nextInt();
+        int in=sc.nextInt();
 
-        // Upper Half
-        for (int i = 1; i <= n; i++) {
-            // Print spaces
-            for (int space = 1; space <= (n - i); space++) {
+        for (int i=1;i<=in ;i++ ) 
+        {
+            for (int sp=1;sp<=(in-i);sp++ ) {
                 System.out.print(" ");
             }
-            // Print stars
-            for (int star = 1; star <= (2 * i - 1); star++) {
-                System.out.print("*");
+            for (int j=1;j<=i ;j++ ) 
+            {
+                System.out.print("* ");
+                
             }
             System.out.println();
         }
 
-        // Lower Half
-        for (int i = n - 1; i >= 1; i--) {
-            // Print spaces
-            for (int space = 1; space <= (n - i); space++) {
+
+
+         for (int i=1;i<=in ;i++ ) 
+        {
+            for (int sp=1;sp<=(in-i);sp++ ) {
                 System.out.print(" ");
             }
-            // Print stars
-            for (int star = 1; star <= (2 * i - 1); star++) {
-                System.out.print("*");
+            for (int j=1;j<=i ;j++ ) 
+            {
+                System.out.print("* ");
+                
             }
             System.out.println();
         }
 
-        sc.close();
+
+
+
+                 for (int i=1;i<=in ;i++ ) 
+        {
+            for (int sp=1;sp<=(in-i);sp++ ) {
+                System.out.print(" ");
+            }
+            for (int j=1;j<=i ;j++ ) 
+            {
+                System.out.print("* ");
+                
+            }
+            System.out.println();
+        }
+
+
+
+                 for (int i=1;i<=in ;i++ ) 
+        {
+            for (int sp=1;sp<=(in-i);sp++ ) {
+                System.out.print(" ");
+            }
+            for (int j=i/2;j<=i ;j++ ) 
+            {
+               // if (i==j) {
+                   System.out.print("* ");
+               // }
+                
+            }
+            System.out.println();
+        }
     }
 }
