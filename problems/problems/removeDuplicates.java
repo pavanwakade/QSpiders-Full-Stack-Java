bfa7e86aad084b48563323a803b6e76fa2347452
pavanwakade <pavanwakade199@gmail.com>
2025-06-19 14:16:56 +0530

    Updated

--- problems/problems/removeDuplicates.java
+++ problems/problems/removeDuplicates.java
@@ -1,26 +1,27 @@
+import java.util.Arrays;
 
 class removeDuplicates {
-    public int[] removeDuplicate(int[] nums) 
-    {
-        int[] r=new int[nums.length];
-       for(int i =0 ;i<nums.length;i++)
-       {
+  public static int[] removeDuplicate(int[] nums) {
+    int[] r = new int[nums.length];
+    int ind=0;
+    r[ind++] = nums[0];
+    for (int i = 0; i < nums.length; i++) {
+    
+
+      for (int j = i; j < nums.length; j++) {
         
-        for(int j= i ;j<nums.length;j++){
-
-               if(nums[i]==nums[j]){
-        r[i]=-1;
-       }
-       else{
-        r[i]=nums[j];
-       }
-   }
-
-        return r; 
+        if (nums[i] == nums[j]) {
+          r[ind++] = -1;
+        } else {
+          r[ind++] = nums[j];
+        }
+      }
     }
+    return r;
+  }
 
-    public static void main(String[] args) {
-        int nums[]={0,0,1,1,1,2,2,3,3,4};
-        removeDuplicate(nums);
-    }
+  public static void main(String[] args) {
+    int nums[] = {0, 0, 1, 1, 1, 2, 2, 3, 3, 4};
+    System.out.println(Arrays.toString(removeDuplicate(nums)));
+  }
 }
