4c05696ddda1d94738d84995d88ddbe0a22f508c
pavanwakade <pavanwakade199@gmail.com>
2025-06-19 12:46:57 +0530

    Updated

--- problems/problems/reverseWithoutConvertArray.java
+++ problems/problems/reverseWithoutConvertArray.java
@@ -1,15 +1,15 @@
 package problems;
 
-class reverseWithoutConvertArray{
-	public static void main(String[] args) {
-	System.out.println(	reverse("pavan"));
-	}
+class reverseWithoutConvertArray {
+  public static void main(String[] args) {
+    System.out.println(reverse("pavan"));
+  }
 
-	public static String reverse(String str){
-		String s="";
-		for (int i=0;i<str.length() ;i++ ) {
-			s+=str.charAt(str.length()-i-1);
-		}
-		return s;
-	}
+  public static String reverse(String str) {
+    String s = "";
+    for (int i = 0; i < str.length(); i++) {
+               s += str.charAt(str.length() - i - 1);
+    }
+    return s;
+  }
 }
