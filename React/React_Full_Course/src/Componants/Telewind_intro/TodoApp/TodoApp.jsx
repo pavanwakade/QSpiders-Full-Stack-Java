32d392ca8578f7a7a9aeb2a7de1ddf8215c4a498
pavanwakade <pavanwakade199@gmail.com>
2025-06-20 14:58:46 +0530

    Updated

--- React/React_Full_Course/src/Componants/Telewind_intro/TodoApp/TodoApp.jsx
+++ React/React_Full_Course/src/Componants/Telewind_intro/TodoApp/TodoApp.jsx
@@ -1,15 +1,17 @@
-import ToHeadbg from './../../../assets/ToDoBg.jpg'
+import ToHeadbg from './../../../assets/ToDoBg.jpg';
+
 export const TodoApp = () => {
   return (
     <>
-<div
-  className="h-[150px] bg-cover bg-center text-white text-xl flex items-center justify-center"
-  style={{back}}
->
-  To Do-List
-</div>
-
+      <div
+        className=" h-[150px] bg-cover bg-center  blur-[1.3px] relative"
+        style={{ backgroundImage: `url(${ToHeadbg}) ` }}
+      ></div>
+        <div className='text-[#ffffff] text-4xl font-extrabold absolute top-[15%] left-[45%]'>To Do List</div>
+       
+    
     </>
-  )
-}
-export default TodoApp
+  );
+};
+
+export default TodoApp;
