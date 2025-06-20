8fc851f14ed2926f8d1a31dac3bd15b1d17dfe66
pavanwakade <pavanwakade199@gmail.com>
2025-06-20 19:57:10 +0530

    Updated

--- React/React_Full_Course/src/Componants/Telewind_intro/TodoApp/TodoApp.jsx
+++ React/React_Full_Course/src/Componants/Telewind_intro/TodoApp/TodoApp.jsx
@@ -1,22 +1,49 @@
 import ToHeadbg from './../../../assets/ToDoBg.jpg';
 
 export const TodoApp = () => {
-  return (
-    <>
-      <div
-        className=" h-[25vh] bg-cover bg-center  blur-[1.3px] relative" style={{ backgroundImage: `url(${ToHeadbg}) ` }}></div>
-        <div className='text-[#ffffff] text-4xl font-extrabold absolute top-[15%] left-[45%]'>To Do List</div>
+    return (
+        <>
+            {/* Header with blurred background */}
+            <div className="relative h-[25vh] w-full overflow-hidden">
+                <div
+                    className="absolute inset-0 bg-cover bg-center filter blur-sm scale-110"
+                    style={{ backgroundImage: `url(${ToHeadbg})` }}
+                ></div>
+                <h1 className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white text-4xl font-extrabold">
+                    To Do List
+                </h1>
+            </div>
 
-        <div className='bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 ... w-[100%] h-[100vh] text-[white] flex justify-center text-center'>
-            <table className='justify-center text-center border w-[50%] mt-10'>
-                <thead>Todos</thead>
-                <thead><button className='bg-[#ff0000bd] p-2'>delete</button></thead>
-                <tbody><input type='text'></input>  </tbody>
-            </table>
-        </div>
-    
-    </>
-  );
+            {/* Main Content */}
+            <div className="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 w-full min-h-[75vh] text-white flex justify-center">
+                <table className="w-[60%] mt-10 text-center border border-white">
+
+                    <tbody>
+                        <tr className="border-t border-white ">
+                            <td className="p-4">
+                                <input
+                                    type="text"
+                                    className="p-2 w-full text-black rounded"
+                                    placeholder="Enter your task"
+                                />
+                            </td>
+                            <td className="">
+                                <button className="bg-[#008000cc] hover:bg-[green] px-4 py-2 rounded">
+                                    add
+                                </button>
+                            </td>
+
+                            <td className="">
+                                <button className="bg-red-600 hover:bg-red-700 px-4 py-2 rounded">
+                                    Delete
+                                </button>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </>
+    );
 };
 
 export default TodoApp;
