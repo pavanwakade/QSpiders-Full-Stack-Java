fe958e468741bebe78a1456311531f5b357539a8
pavanwakade <pavanwakade199@gmail.com>
2025-06-21 18:53:24 +0530

    Updated

--- CorejavaOOPs/pattern/p.java
+++ CorejavaOOPs/pattern/p.java
@@ -1,3 +1,5 @@
+package pattern;
+
 public class p {
     public static void main(String[] args) {
         int n = 7; // height of each letter
--- Java_Daily_Project/src/StudentManagement/StudentsCRUD.java
+++ Java_Daily_Project/src/StudentManagement/StudentsCRUD.java
@@ -11,6 +11,7 @@ import java.util.List;
  */
 public class StudentsCRUD {
 	static List<Students> students = new LinkedList<>();
+
 	public static void main(String[] args) {
 
 		students.add(new Students(1, "pavan", 100.0));
@@ -19,19 +20,48 @@ public class StudentsCRUD {
 		students.add(new Students(4, "Savan", 50.0));
 		students.add(new Students(5, "Ram", 100.0));
 		getAllStudents();
-
+		System.out.println(getStudentByRollNo(33));
+		
+		System.out.println(updateStudent(4, "pavan", 100.0));
+		getAllStudents();
 	}
-	 
-	 
+
 	public static void getAllStudents() {
 		for (Students students2 : students) {
 			System.out.println(students2);
 		}
 	}
-	
-	public  Students getStudentByRollNo(int rollNo) {
-		 if (students.rollNo==rollNo) {
-			
+
+	public static Students getStudentByRollNo(int rollNo) {
+
+		for (Students students2 : students) {
+
+			if (students2.getRollNo() == rollNo) {
+				return students2;
+			}
 		}
+		return null;
+	}
+
+	static boolean updateStudent(int rollNo, String newName, double newMarks) {
+
+		for (Students students2 : students) {
+			if (students2.getRollNo() == rollNo) {
+
+				students2.setName(newName);
+				students2.setMarks(newMarks);
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	
+	 boolean deleteStudent(int rollNo) {
+		 
+
+		 
+		return false;
+		 
 	 }
 }
