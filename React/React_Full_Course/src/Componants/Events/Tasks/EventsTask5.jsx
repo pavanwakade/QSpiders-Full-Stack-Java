f3d9d9a7cf156a2ef16a831a8dc291084d5dcf82
pavanwakade <pavanwakade199@gmail.com>
2025-06-24 18:41:02 +0530

    Updated

--- React/React_Full_Course/src/Componants/Events/Tasks/EventsTask5.jsx
+++ React/React_Full_Course/src/Componants/Events/Tasks/EventsTask5.jsx
@@ -4,11 +4,11 @@ const EventsTask5 = () => {
     <>
     <div>
       <ol className='bg-[#ddc7c7] p-9 text-center text-2xl font-bold'>
-        <li onMouseOver={(e)=>{e.target.style.backgroundColor="red"}} onMouseLeave={(e)=>{e.target.style.backgroundColor=""}}>Home</li>
-        <li onMouseOver={(e)=>{e.target.style.backgroundColor="red"}} onMouseLeave={(e)=>{e.target.style.backgroundColor=""}}>about</li>
-        <li onMouseOver={(e)=>{e.target.style.backgroundColor="red"}} onMouseLeave={(e)=>{e.target.style.backgroundColor=""}}>Services</li>
-        <li onMouseOver={(e)=>{e.target.style.backgroundColor="red"}} onMouseLeave={(e)=>{e.target.style.backgroundColor=""}}>Contact</li>
-        <li onMouseOver={(e)=>{e.target.style.backgroundColor="red"}} onMouseLeave={(e)=>{e.target.style.backgroundColor=""}}>Blog</li>
+        <li onMouseOver={(e)=>{e.target.style.backgroundColor="red"; e.target.style.cursor="pointer";}} onMouseLeave={(e)=>{e.target.style.backgroundColor=""}}>Home</li>
+        <li onMouseOver={(e)=>{e.target.style.backgroundColor="red";e.target.style.cursor="pointer";}} onMouseLeave={(e)=>{e.target.style.backgroundColor=""}}>about</li>
+        <li onMouseOver={(e)=>{e.target.style.backgroundColor="red";e.target.style.cursor="pointer";}} onMouseLeave={(e)=>{e.target.style.backgroundColor=""}}>Services</li>
+        <li onMouseOver={(e)=>{e.target.style.backgroundColor="red";e.target.style.cursor="pointer";}} onMouseLeave={(e)=>{e.target.style.backgroundColor=""}}>Contact</li>
+        <li onMouseOver={(e)=>{e.target.style.backgroundColor="red";e.target.style.cursor="pointer";}} onMouseLeave={(e)=>{e.target.style.backgroundColor=""}}>Blog</li>
       </ol>
     </div>
     </>
