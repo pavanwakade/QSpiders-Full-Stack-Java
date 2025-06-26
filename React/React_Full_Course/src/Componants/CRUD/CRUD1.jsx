39e760878f54be78cff24d3dee99644403ac866a
pavanwakade <pavanwakade199@gmail.com>
2025-06-26 14:36:37 +0530

    Updated

--- React/React_Full_Course/src/Componants/CRUD/CRUD1.jsx
+++ React/React_Full_Course/src/Componants/CRUD/CRUD1.jsx
@@ -13,43 +13,43 @@ const CRUD = () => {
 
     return (
         <>CRUD
-<div>
+            <div>
 
-            <form >
-                <input type='text'
-                    placeholder='enter name'
-                    onChange={(e) => { setUser(e.target.value) }}
-                    className="text-center border border-[gray]"
-                />
-                <br /><br />
+                <form >
+                    <input type='text'
+                        placeholder='enter name'
+                        onChange={(e) => { setUser(e.target.value) }}
+                        className="text-center border border-[gray]"
+                    />
+                    <br /><br />
 
-                <input type='text'
-                    placeholder='enter password'
-                    onChange={(e) => { setPass(e.target.value) }}
-                    className="text-center border border-[gray]"
-                />
+                    <input type='text'
+                        placeholder='enter password'
+                        onChange={(e) => { setPass(e.target.value) }}
+                        className="text-center border border-[gray]"
+                    />
 
-                <br /> <br />
+                    <br /> <br />
 
-                <button onClick={handleSubmit}    className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-4 rounded mr-2">signup</button>
+                    <button onClick={handleSubmit} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-4 rounded mr-2">signup</button>
 
-            </form>
-                </div>
+                </form>
+            </div>
 
             <div >
                 {
                     list.map((val, i) => (
                         <div key={i}>
-                           <span> {val.userName} </span> 
-                           <span>{val.password}</span>
+                            <span> {val.userName} </span>
+                            <span>{val.password}</span>
                             <br />
-                           <button
-  className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-4 rounded mr-2">edit</button>
-<button
-  className="bg-red-500 hover:bg-red-700 text-white font-bold py-1 px-4 rounded"
->
-  delete
-</button>
+                            <button
+                                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-4 rounded mr-2">edit</button>
+                            <button
+                                className="bg-red-500 hover:bg-red-700 text-white font-bold py-1 px-4 rounded"
+                            >
+                                delete
+                            </button>
                         </div>
                     ))
                 }
