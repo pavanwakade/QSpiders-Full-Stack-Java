118d2f8b1c38f3fc4a35f519bfe60631f6ca31bc
pavanwakade <pavanwakade199@gmail.com>
2025-06-27 00:19:41 +0530

    Updated

--- problems/dailyProblmes/IntersectionOfTwoArrays.java
+++ problems/dailyProblmes/IntersectionOfTwoArrays.java
@@ -7,5 +7,18 @@ package dailyProblmes;
  * 
  */
 public class IntersectionOfTwoArrays {
+	public static void main(String[] args) {
+		int[] a = { 1, 2, 3, 4, 5, 6 };
+		int[] b = { 4, 5, 6, 7, 8, 9, 4, 5, 6 };
 
+		for (int i = 0; i < a.length; i++) {
+
+			for (int j = 0; j < b.length; j++) {
+				if (a[i] == b[j]) {
+					System.out.println(a[i]);
+				}
+			}
+
+		}
+	}
 }
