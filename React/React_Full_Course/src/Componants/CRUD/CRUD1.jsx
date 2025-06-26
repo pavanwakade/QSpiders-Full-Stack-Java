f0a1f266f61950eb80938f562e11c627c8dde39c
pavanwakade <pavanwakade199@gmail.com>
2025-06-26 11:44:10 +0530

    Updated

--- React/React_Full_Course/src/Componants/CRUD/CRUD1.jsx
+++ React/React_Full_Course/src/Componants/CRUD/CRUD1.jsx
@@ -13,29 +13,35 @@ const CRUD = () => {
 
     return (
         <>CRUD
+<div>
 
-            <form>
+            <form >
                 <input type='text'
                     placeholder='enter name'
                     onChange={(e) => { setUser(e.target.value) }}
+                    className="text-center border border-[gray]"
                 />
                 <br /><br />
 
                 <input type='text'
                     placeholder='enter password'
                     onChange={(e) => { setPass(e.target.value) }}
+                    className="text-center border border-[gray]"
                 />
 
                 <br /> <br />
 
-                <button onClick={handleSubmit}>signup</button>
+                <button onClick={handleSubmit}    className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-4 rounded mr-2">signup</button>
 
             </form>
+                </div>
 
-            <div>
+            <div >
                 {
                     list.map((val, i) => (
-                        <div key={i}>{val.userName}  {val.password}
+                        <div key={i}>
+                           <span> {val.userName} </span> 
+                           <span>{val.password}</span>
                             <br />
                            <button
   className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-4 rounded mr-2">edit</button>
