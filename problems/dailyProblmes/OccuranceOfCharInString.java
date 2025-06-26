c38fd46730992043bd95c363f0ba4251468e5038
pavanwakade <pavanwakade199@gmail.com>
2025-06-26 22:06:28 +0530

    Updated

--- problems/dailyProblmes/OccuranceOfCharInString.java
+++ problems/dailyProblmes/OccuranceOfCharInString.java
@@ -6,19 +6,57 @@ import java.util.Arrays;
  * 
  */
 public class OccuranceOfCharInString {
-	
+
 	public static void main(String[] args) {
-		
-		String s="hello pavan";
-		
-		 char [] sChar=s.toCharArray();
-		 
-		 for (int i = 0; i < sChar.length; i++) {
-			 
-			 
+
+		String s = "hello pavan";
+
+		char[] sChar = s.toCharArray();
+
+		for (int i = 0; i < sChar.length; i++) {
+
+			if (sChar[i] == ' ') {
+				continue;
+			}
+
+			boolean isDuplicate = false;
+
+			for (int j = 0; j < i; j++) {
+				if (sChar[i] == sChar[j]) {
+					isDuplicate = true;
+					break;
+
+				}
+			}
+			if (!isDuplicate) {
+				int c = 0;
+				for (int k = 0; k < sChar.length; k++) {
+
+					if (sChar[i] == sChar[k]) {
+						c++;
+					}
+				}
+				System.out.println(sChar[i] + "=" + c);
+			}
+
 		}
-		 
-		 System.out.println(Arrays.toString(sChar));
+
+		System.out.println(Arrays.toString(sChar));
+		
+		
+		for (int i = 0; i < sChar.length; i++) {
+			if (sChar[i]==' ') {
+				continue;
+			}
+			int ct=0;
+			for (int j = 0; j < sChar.length; j++) {
+				if (sChar[i]==sChar[j]) {
+					
+				ct++;	
+				}
+			}
+			System.out.println(sChar[i]+" = "+ct);
+		}
 	}
 
 }
