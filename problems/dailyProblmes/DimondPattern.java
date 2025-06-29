72bddfa4b6b30a27f3fdb6343e0142ef2c7ac3ee
pavanwakade <pavanwakade199@gmail.com>
2025-06-29 11:24:09 +0530

    Updated

--- problems/dailyProblmes/DimondPattern.java
+++ problems/dailyProblmes/DimondPattern.java
@@ -11,23 +11,28 @@ import java.util.Scanner;
 public class DimondPattern {
 
 	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
-		System.out.println("enter no");
-		int input = scanner.nextInt();
+//		Scanner scanner = new Scanner(System.in);
+//		System.out.println("enter no");
+//		int input = scanner.nextInt();
+		int input = 10;
 
 		for (int i = 0; i < input; i++) {
-			for (int j = 0; j < i-1; j++) {
-				System.out.print(" ");
-			}
-				
+			
 			for (int j = 0; j < input; j++) {
-				System.out.print("* ");
 				
+				if (i==0 || j==0 || i==input-1 || j==input-1 || i==j || i==input/2 || ) {
+					
+					System.out.print("* ");
+				}
+				else { 
+					System.out.print("  ");
+				}
+				 
 			}
 			System.out.println();
-			}
 			
-			
+		}
+
 	}
 
 }
