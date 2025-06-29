c0983cef2ad79d6bcdb282e7f4302978b21a23ec
pavanwakade <pavanwakade199@gmail.com>
2025-06-29 18:30:36 +0530

    Updated

--- problems/dailyProblmes/ReverseStringWords.java
+++ problems/dailyProblmes/ReverseStringWords.java
@@ -6,7 +6,8 @@ package dailyProblmes;
 import java.util.Arrays;
 
 /**
- * simple inpiut String str="i Love Java i like java"; expected op -> i evoL avaJ i ekil avaJ
+ * simple inpiut String str="i Love Java i like java"; expected op -> i evoL
+ * avaJ i ekil avaJ
  */
 public class ReverseStringWords {
 
@@ -20,19 +21,24 @@ public class ReverseStringWords {
 
 	public static String ReverseWords(String str) {
 
-		
 		String s = "";
 
 		String[] strArr = str.split(" ");
 		for (String word : strArr) {
-			String reversedwordString="";
-			for (int i = 0; i < strArr.length; i++) {
-				
-			}
-			
-		}
-		System.out.println(ssString);
-		return ssString;
 
+			String reversedwordString = "";
+
+			String[] c = word.split("");
+
+			for (int i = c.length - 1; i >= 0; i--) {
+
+				reversedwordString += c[i];
+			}
+			s += reversedwordString + " ";
+
+//			System.out.println(s);
+		}
+		System.out.println(s);
+		return s;
 	}
 }
