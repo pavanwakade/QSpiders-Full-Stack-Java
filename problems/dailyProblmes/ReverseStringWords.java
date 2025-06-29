e75747ad11d736d349ec32eba423c4685cd96ca9
pavanwakade <pavanwakade199@gmail.com>
2025-06-29 14:24:09 +0530

    Updated

--- problems/dailyProblmes/ReverseStringWords.java
+++ problems/dailyProblmes/ReverseStringWords.java
@@ -3,9 +3,36 @@
  */
 package dailyProblmes;
 
+import java.util.Arrays;
+
 /**
- * 
+ * simple inpiut String str="i Love Java i like java"; expected op -> i evoL avaJ i ekil avaJ
  */
 public class ReverseStringWords {
 
+	public static void main(String[] args) {
+
+		String str = "i Love Java i like java";
+
+		ReverseWords(str);
+
+	}
+
+	public static String ReverseWords(String str) {
+
+		
+		String s = "";
+
+		String[] strArr = str.split(" ");
+		for (String word : strArr) {
+			String reversedwordString="";
+			for (int i = 0; i < strArr.length; i++) {
+				
+			}
+			
+		}
+		System.out.println(ssString);
+		return ssString;
+
+	}
 }
