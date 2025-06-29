a92092de39dc8c6eb76590929242068b726bee2c
pavanwakade <pavanwakade199@gmail.com>
2025-06-29 11:54:09 +0530

    Updated

--- problems/dailyProblmes/DimondPattern.java
+++ problems/dailyProblmes/DimondPattern.java
@@ -3,6 +3,7 @@
  */
 package dailyProblmes;
 
+import java.util.Iterator;
 import java.util.Scanner;
 
 /**
@@ -14,24 +15,51 @@ public class DimondPattern {
 //		Scanner scanner = new Scanner(System.in);
 //		System.out.println("enter no");
 //		int input = scanner.nextInt();
-		int input = 10;
-		int num=0;
+		int input = 11;
 
 		for (int i = 0; i < input; i++) {
-			
-			for (int j = 0; j < input; j++) {
-				
-				if (i==0 || j==0 || i==input-1 || j==input-1 || i==j || i==input/2 ||j==input-i-1) {
-					
-					System.out.print(++num+" ");
-				}
-				else { 
-					System.out.print("  ");
-				}
-				 
+
+			for (int j = 0; j <= i; j++) {
+				System.out.print("* ");
 			}
 			System.out.println();
-			
+		}
+
+		System.out.println();
+
+		
+		
+		
+		for (int i = input; i >= 0; i--) {
+
+			for (int j = 0; j <= i; j++) {
+				System.out.print("* ");
+			}
+			System.out.println();
+
+		}
+
+		System.out.println();
+		
+		
+		
+		
+
+		for (int i = 0; i < input; i++) {
+
+			for (int j = 0; j < input; j++) {
+
+				if (i == 0 || j == 0 || i == input - 1 || j == input - 1 || i == j || i == input / 2 || j == input / 2
+						|| j == input - i - 1) {
+
+					System.out.print("* ");
+				} else {
+					System.out.print("  ");
+				}
+
+			}
+			System.out.println();
+
 		}
 
 	}
