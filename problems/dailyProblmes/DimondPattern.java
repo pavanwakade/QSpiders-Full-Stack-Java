60e9b0e9f5011b2ee076a12e9dc17d68a87bc69a
pavanwakade <pavanwakade199@gmail.com>
2025-06-29 10:54:11 +0530

    Updated

--- problems/dailyProblmes/DimondPattern.java
+++ problems/dailyProblmes/DimondPattern.java
@@ -15,16 +15,19 @@ public class DimondPattern {
 		System.out.println("enter no");
 		int input = scanner.nextInt();
 
-		for (int i = 0; i <= input; i++) {
-			for (int j = 0; j <= i - input; j++) {
+		for (int i = 0; i < input; i++) {
+			for (int j = 0; j < i-1; j++) {
 				System.out.print(" ");
 			}
-			
-			for (int k = 0; k <=i  ; k++) {
+				
+			for (int j = 0; j < input; j++) {
 				System.out.print("* ");
+				
 			}
 			System.out.println();
-		}
+			}
+			
+			
 	}
 
 }
