be8d6f950d2ff19cf194cb6dd3293903d0e3e66c
pavanwakade <pavanwakade199@gmail.com>
2025-07-01 11:42:54 +0530

    Updated

--- React/React_Full_Course/src/Componants/ContextAPI/ContextAPI.jsx
+++ React/React_Full_Course/src/Componants/ContextAPI/ContextAPI.jsx
@@ -1,7 +1,15 @@
+import Strore from "./Strore"
+import Subcomponents from "./SubComponents/Subcomponents"
 
 const ContextAPI = () => {
   return (
+    <>
     <div>ContextAPI</div>
+
+    <Strore>
+      <Subcomponents />
+    </Strore>
+    </>
   )
 }
 
