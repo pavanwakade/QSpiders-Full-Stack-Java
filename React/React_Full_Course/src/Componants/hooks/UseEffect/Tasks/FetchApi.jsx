2ccdf91ec0c0cf7d3e68123f6f39a7f9b0425cca
pavanwakade <pavanwakade199@gmail.com>
2025-07-02 12:30:35 +0530

    Updated

--- React/React_Full_Course/src/Componants/hooks/UseEffect/Tasks/FetchApi.jsx
+++ React/React_Full_Course/src/Componants/hooks/UseEffect/Tasks/FetchApi.jsx
@@ -1,19 +1,48 @@
-import React, { useState } from 'react'
+import { useEffect, useState } from 'react';
 
 const FetchApi = () => {
 
-let [state, setState] = useState(0);
-let [api,setApi]=useState([]);
+  let [state, setState] = useState(0);
+  let [api, setApi] = useState([]);
 
-let fetchDate=async ()=>{
-  let response=await fetch(`https://api.github.com/users`);
-  let data=await response.json();
-  setApi(data);
-}
+  // let data;
+  let fetchDate = async () => {
+    let response = await fetch(`https://api.github.com/users`);  //it call only once 
+    console.log(response);
+    let data = await response.json();
+    setApi(data);
+    console.log("api called");
+
+
+  }
+  console.log(api);
+
+  useEffect(() => {
+    fetchDate();
+  }, [])
+
+  
+
 
   return (
-    <>FetchApi
+    <>
+      <button onClick={() => { setState(state + 1) }}>click : {state}</button>
 
+      <div>
+        {
+          api.map((item) => {
+            return (
+              <div key={item.id}>
+                <h1>{item.login}</h1>
+                <img src={item.avatar_url} alt={item.login} className='w-[80%]' />
+              </div>
+            );
+          })
+        }
+      </div>
+      <div>
+        {}
+      </div>
 
     </>
   )
