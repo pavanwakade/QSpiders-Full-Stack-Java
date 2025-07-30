b4c275494ebd888434ace23f21c4243065e515f0
pavanwakade <pavanwakade199@gmail.com>
2025-07-30 17:47:55 +0530

    Updated

--- React/React_Full_Course/src/Assignments/CRUD/ClassTasks/ToDoList.jsx
+++ React/React_Full_Course/src/Assignments/CRUD/ClassTasks/ToDoList.jsx
@@ -1,62 +1,34 @@
-// import React, { useState } from 'react';
-
-// const ToDoList = () => {
-//   const [tasks, setTasks] = useState([]);
-//   const [newTask, setNewTask] = useState('');
-
-//   const handleInputChange = (event) => {
-//     setNewTask(event.target.value);
-//   };
-
-//   const addTask = () => {
-//     if (newTask.trim() !== '') {
-//       setTasks([...tasks, newTask]);
-//       setNewTask('');
-//     }
-//   };
-
-//   return (
-//     <div>
-//       <input type="text" value={newTask} onChange={handleInputChange} />
-//       <button onClick={addTask}>Add Task</button>
-//       <ul>
-//         {tasks.map((task, index) => (
-//           <li key={index}>{task}</li>
-//         ))}
-//       </ul>
-//     </div>
-//   );
-// };
-
-// export default ToDoList;
-
-
-
-
-
-
 import React, { useState } from 'react'
 
 const ToDoList = () => {
   let [task, setTasks] = useState([]);
+  let [newTask, setNewTask] = useState('');
   let handleSubmit = () => {
-    setTasks([...task, task])
+    setTasks([...task, newTask])
+    setNewTask('')
   }
   return (
-    <div className='flex gap-4'>
+    <div >
 
-      <form>
+      <form className='flex gap-4'>
 
-        <input type="text" value={task} placeholder='Enter New Task'
-          onChange={(e) => setTasks(e.target.value)}
+        <input type="text" value={newTask} placeholder='Enter New Task'
+          onChange={(e) => setNewTask(e.target.value)}
         />
         <button type="button" onClick={() => handleSubmit()}>add Task</button>
       </form>
-      {
-        task.map((val, i) => (
-          <div key={i}>{val}</div>
-        ))
-      }
+      <div className='felx'>
+        {
+          task.map((val, i) => (
+            <div key={i}>
+              <div>{val}</div>
+              <button type="button">update</button>
+              <button type="button">delete</button>
+
+            </div>
+          ))
+        }
+      </div>
     </div>
   )
 }
