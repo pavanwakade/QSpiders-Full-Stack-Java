3cba3d823150b3229e5ce23992f7e39884564d4f
pavanwakade <pavanwakade199@gmail.com>
2025-08-24 13:11:54 +0530

    Updated

--- React/Projects/Learning_Hub/src/Pages/Login.jsx
+++ React/Projects/Learning_Hub/src/Pages/Login.jsx
@@ -1,23 +1,43 @@
 import React, { useState } from 'react'
+import Swal from 'sweetalert2'
 
 const Login = () => {
-    let[email,setEmail]=useState('')
-    let[password,setPassword]=useState('')
-    let handleLogin=()=>{
-        if (email==='pavan@gmail.com' && password==='pavan') {
-            alert("Login sucessfull")
+    let [email, setEmail] = useState('')
+    let [password, setPassword] = useState('')
+    let handleLogin = () => {
+        if (email === 'pavan@gmail.com' && password === 'pavan') {
+            Swal.fire({
+                title: 'Login Successful!',
+                text: 'Welcome back!',
+                icon: 'success',
+                confirmButtonColor: '#8b5cf6', // purple button
+                confirmButtonText: 'OK'
+            });
+        }
+        else {
+            Swal.fire({
+                title: 'Login Failed',
+                text: 'Email or password is incorrect!',
+                icon: 'error',
+                confirmButtonColor: '#8b5cf6', // purple button
+                confirmButtonText: 'Try Again'
+            });
         }
     }
-  return (
-    <div>
-      <form method="post">
-     <h5 id='inputTitle'>Email</h5>  <input type="email" value={email} onChange={(e)=>setEmail(e.target.value)} placeholder='Email' /> <br />
-     <h5 id='inputTitle'>Password</h5>       <input type="password" value={password} onChange={(e)=>setPassword(e.target.value)} placeholder='Password' />
-     <br />
-        <button type="submit" onClick={handleLogin}>Login</button>
-      </form>   
-       </div>
-  )
+    return (
+        <div>
+            <form className='flex flex-col items-center justify-center h-screen gap-3'>
+                <span> <p id='inputTitle'>Email</p>
+                    <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder='Email' />
+                </span>
+                <span>
+                    <p id='inputTitle'>Password</p>
+                    <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder='Password' />
+                </span>
+                <button type="submit" onClick={handleLogin} id='loginbutton'>Login</button>
+            </form>
+        </div>
+    )
 }
 
 export default Login
