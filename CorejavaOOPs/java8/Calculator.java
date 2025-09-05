bea65c3ff05442e0fbcdc7803a8fcc20da736b28
pavanwakade <pavanwakade199@gmail.com>
2025-09-05 10:33:55 +0530

    Updated

--- CorejavaOOPs/java8/Calculator.java
+++ CorejavaOOPs/java8/Calculator.java
@@ -1,48 +1,14 @@
 package java8;
 
-//
-//@FunctionalInterface
-//interface calculate {
-//	int a(int a, int b);
-//    static double a(double a,double b) {
-//		return a+b;
-//	} 
-//}
-//
-//public class Calculator {
-//	public static void main(String[] args) {
-//
-//		calculate add = (a, b) -> a + b;
-//		calculate sub = (a, b) -> a - b;
-//		calculate mul = (a, b) -> a * b;
-//		calculate div = (a, b) -> a == 0 || b == 0 ? 0 : a / b;
-//
-//		System.out.println("addition      : " + add.a(10, 1));
-//		System.out.println("substraction  : " + sub.a(4, 5));
-//		System.out.println("Multiplication: " + mul.a(4, 5));
-//		System.out.println("division      : " + div.a(4, 5));
-//		System.out.println("division      : " + div.a(4, 0));
-//		
-//	}
-//}
 
-// Using Verags
-@FunctionalInterface
-interface CalInterface {
-	double calculate(double... nums);
-}
+class Calculator {
+    public static void main(String[] args) {
 
-public class Calculator {
-	public static void main(String[] args) {
-		CalInterface add = (double... nums) -> {
-			double sum = 0;
-			for (double n : nums) {
-				sum += n;
-			}
-			return sum;
-		};
-		System.out.println(add.calculate(10, 20)); // 30.0
-		System.out.println(add.calculate(1, 2, 3, 4, 5, 1, 2, 3, 4, 5));// 30.0
-		System.out.println(add.calculate()); // 0.0
-	}
+        System.out.println(add(4, 6));
+
+    }
+
+    static int add(int a, int b) {
+        return a + b;
+    }
 }
