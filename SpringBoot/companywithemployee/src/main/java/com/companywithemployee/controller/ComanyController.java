064929e3d8f11ccabb62e6628f886aea44442023
pavanwakade <pavanwakade199@gmail.com>
2025-09-28 10:33:40 +0530

    Updated

---
+++ SpringBoot/companywithemployee/src/main/java/com/companywithemployee/controller/ComanyController.java
@@ -0,0 +1,120 @@
+package com.companywithemployee.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.companywithemployee.entity.Company;
+import com.companywithemployee.entity.Employee;
+import com.companywithemployee.repository.CompanyRepository;
+import com.companywithemployee.repository.EmployeeRepository;
+import com.companywithemployee.service.Companyservice;
+
+@RestController
+public class ComanyController {
+
+	@Autowired
+	private Companyservice service;
+
+	@Autowired
+	private CompanyRepository companyrepository;
+	
+	
+	@Autowired
+	private EmployeeRepository emprepository;
+	
+	
+	@Autowired
+	private JavaMailSender javaMailSender;
+	
+	
+	
+	@PostMapping("/company")
+	public ResponseEntity<Company> saveCompany(@RequestBody Company company) {
+		
+		return service.saveCompany(company);
+	}
+	
+	
+	
+	
+	
+	@PatchMapping("/company/{companyid}/{employeeid}")
+	public void mapcompanyToEmployee(@PathVariable int companyid,@PathVariable int employeeid) {
+		
+		Company company=companyrepository.findById(companyid).orElse(null);	
+		if (company == null) {
+			
+			return;
+		}
+		
+		Employee employee=emprepository.findById(employeeid).orElse(null);
+		
+		if (employee == null) {
+			return;
+		}
+		
+		employee.setCompany(company);
+		
+		emprepository.save(employee);
+	}
+
+	
+	
+	
+	
+	
+	@PostMapping("/updateCompany")
+	public ResponseEntity<Company> updateCompany(Company company) {
+		return service.saveCompany(company);
+	}
+
+	
+	@GetMapping("/findcompanybyid")
+	public ResponseEntity<Company> findCompanyById(@RequestParam int id) {
+		return service.findCompanyById(id);
+	}
+
+	
+	@GetMapping("/findallcompanies")
+	public ResponseEntity<List<Company>> getAllCompanies() {
+		return service.findAllCompanies();
+	}
+
+	
+	@DeleteMapping("/deletecompany/{id}")
+	public ResponseEntity<Company> deleteEmployeeById(@PathVariable int id) {
+		return service.findCompanyById(id);
+	}
+	
+	
+	
+	
+	@PostMapping("/mail/{to}/{subject}/{text}")
+	public void sendMail(@PathVariable String to,@PathVariable String subject,@PathVariable String text)
+	{
+		SimpleMailMessage mailMessage = new SimpleMailMessage();
+		mailMessage.setTo(to);
+		mailMessage.setSubject(subject);
+		mailMessage.setText(text);
+		
+		javaMailSender.send(mailMessage);
+		
+	}
+	
+	
+	
+	
+}
