48baf5ec393964c5c382fb1cbf5820093a59b633
pavanwakade <pavanwakade199@gmail.com>
2025-09-30 20:05:51 +0530

    Updated

---
+++ SpringBoot/InventryManagmentSystem/inventrymanagementsystem/src/test/java/com/inventrymanagmentsytem/inventrymanagementsystem/InventrymanagementsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inventrymanagmentsytem.inventrymanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventrymanagementsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
