195d1a1351da67161b93ff7a8e2b00df08badc63
pavanwakade <pavanwakade199@gmail.com>
2025-10-01 13:54:33 +0530

    Updated

--- SpringBoot/Ecommers/ecommers/src/main/java/com/ecommersbypavan/ecommers/repository/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ecommersbypavan.ecommers.repository;
-
-import com.ecommersbypavan.ecommers.model.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-}
