1cd59defa5c4d4a14a14dcfa2e8e0e4d8869bc15
pavanwakade <pavanwakade199@gmail.com>
2025-10-15 18:22:13 +0530

    Updated

--- hibernate/Postgresql/src/main/java/hibernate_test/DriverDao.java
+++ hibernate/Postgresql/src/main/java/hibernate_test/DriverDao.java
@@ -5,15 +5,11 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Scanner;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.EntityTransaction;
-import javax.persistence.Persistence;
-import javax.persistence.Query;
+
 
 public class DriverDao {
 
-	static EntityManagerFactory emf = Persistence.createEntityManagerFactory("foodapp");
+	static EntityManagerFactory emf = Persistence.createEntityManagerFactory("pavan");
 	static EntityManager em = emf.createEntityManager();
 	static EntityTransaction et = em.getTransaction();
 
