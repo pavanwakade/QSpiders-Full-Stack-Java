4b60845ef32388e06e8996a84fb535a8023e88d4
pavanwakade <pavanwakade199@gmail.com>
2025-11-05 12:17:17 +0530

    Updated

--- React/React_Full_Course/src/Redux/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import cartSlice from './slice.js' // <-- fixed import path
-
-export let store = configureStore({
-    reducer: {
-        cartItems: cartSlice
-    }
-})
